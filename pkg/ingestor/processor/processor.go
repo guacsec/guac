7334096cccf19e2ddc1ee321722556d3b09bd355
Brandon Lum <lumjjb@gmail.com>
2022-08-04 16:40:27 -0400

    Add processorInput structs
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- pkg/ingestor/processor/processor.go
+++ pkg/ingestor/processor/processor.go
@@ -15,4 +15,52 @@
 
 package processor
 
+import (
+	"github.com/secure-systems-lab/go-securesystemslib/dsse"
+	rtype "github.com/sigstore/rekor/pkg/generated/models"
+)
+
 type Processor interface{}
+
+// ProcessorInput describes the input for a processor to run. This input can
+// come from a collector or from the processor itself (run recursively).
+type ProcessorInput struct {
+	Blob              []byte
+	Type              DocumentType
+	Format            FormatType
+	TrustInformation  TrustInformation
+	SourceInformation SourceInformation
+}
+
+// DocumentType describes the type of the document contents for schema checks
+type DocumentType string
+
+// Document* is the enumerables of DocumentType
+const (
+	DocumentSLSA DocumentType = "SLSA"
+	DocumentITE6              = "ITE6"
+	DocumentDSSE              = "DSSE"
+)
+
+// FormatType describes the document format for malform checks
+type FormatType string
+
+// Format* is the enumerables of FormatType
+const (
+	FormatJSON FormatType = "JSON"
+)
+
+// TrustInformation provides additional information about how to verify the document
+type TrustInformation struct {
+	DSSE            *dsse.Envelope
+	LogVerification *rtype.LogEntryAnonVerification
+	IssuerUri       *string
+}
+
+// TrustInformation provides additional information about where the document comes from
+type SourceInformation struct {
+	// Collector describes the name of the collector providing this information
+	Collector string
+	// Source describes the source which the collector got this information
+	Source string
+}
