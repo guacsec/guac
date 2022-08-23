91b9e3069a591e89ae43de21a9e30007fd0a5c4b
Mihai Maruseac <mihaimaruseac@google.com>
2022-08-23 10:31:12 -0700

    Fix minor issues:
    - Replace leftover AFF with GUAC
    - Fix a minor typo.
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- internal/testing/ingestor/simpledoc/simpledoc.go
+++ internal/testing/ingestor/simpledoc/simpledoc.go
@@ -1,5 +1,5 @@
 //
-// Copyright 2022 The AFF Authors.
+// Copyright 2022 The GUAC Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ import (
 
 // TODO: Move to internal
 
-// SimpleDocProc is a document processor for a simple docment
+// SimpleDocProc is a document processor for a simple document
 // It is of JSON blob type
 //
 // It should have an issuer field, that should match up with
