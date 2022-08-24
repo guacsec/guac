6539dafd800e2001c92c54f285b5364c28e6d56e
Brandon Lum <lumjjb@gmail.com>
2022-08-24 10:51:39 -0400

    fix copyright headers
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- cmd/collector/cmd/example.go
+++ cmd/collector/cmd/example.go
@@ -1,5 +1,5 @@
 //
-// Copyright 2022 The AFF Authors.
+// Copyright 2022 The GUAC Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
