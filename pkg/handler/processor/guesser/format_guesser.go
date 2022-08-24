f874ad132d05582fc4b75f0804046e744554b292
Brandon Lum <lumjjb@gmail.com>
2022-08-24 16:54:16 -0400

    add document guesser foundations
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

---
+++ pkg/handler/processor/guesser/format_guesser.go
@@ -0,0 +1,39 @@
+//
+// Copyright 2022 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package guesser
+
+import (
+	"github.com/guacsec/guac/pkg/handler/processor"
+	"github.com/sirupsen/logrus"
+)
+
+// DocumentFormatGuesser guesses the format of the document given a blob
+type DocumentFormatGuesser interface {
+	// GuessFormat returns the format type guessed to processor.FormatUnknown if
+	// it is unable to.
+	GuessFormat(blob []byte) processor.FormatType
+}
+
+var (
+	documentFormatGuessers = map[string]DocumentFormatGuesser{}
+)
+
+func RegisterDocumentFormatGuesser(g DocumentFormatGuesser, name string) {
+	if _, ok := documentFormatGuessers[name]; ok {
+		logrus.Warnf("the document type guesser is being overwritten: %s", name)
+	}
+	documentFormatGuessers[name] = g
+}
