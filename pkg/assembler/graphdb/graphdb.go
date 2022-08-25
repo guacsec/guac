e69f1da988ab0dc693b13489d2a6b9f7d34c2530
Mihai Maruseac <mihaimaruseac@google.com>
2022-08-25 12:25:27 -0700

    Fix typo, fix godoc, separate testing utils
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- pkg/assembler/graphdb/graphdb.go
+++ pkg/assembler/graphdb/graphdb.go
@@ -22,28 +22,23 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
-// Authentification token for connecting to the graph database. Only needed
-// during initial connection setup.
-// Use the `CreateAuthToken...` functions to create a token.
+// AuthToken is the authentication token needed for connecting to the graph
+// database. Use the `CreateAuthToken...` functions to create a token.
 type AuthToken = neo4j.AuthToken
 
-// Creates a simple authentication token with username, password and authentication realm.
+// CreateAuthTokenWithUsernameAndPassword creates a simple authentication token
+// with username, password and authentication realm. This is the method to call
+// in most scenarios when you need an `AuthToken`.
 func CreateAuthTokenWithUsernameAndPassword(username string, password string, realm string) AuthToken {
 	return neo4j.BasicAuth(username, password, realm)
 }
 
-// Creates an empty authentication token.
-// Use only for testing!
-func CreateAuthTokenForTesting() AuthToken {
-	return neo4j.NoAuth()
-}
-
-// Client for connecting to the graph database.
+// Client represents a client to the graph database.
 // TODO(mihaimaruseac): Switch to v5 and `...WithContext` API when v5 is released.
 type Client = neo4j.Driver
 
-// Creates a new connection to the graph database given by `uri`, performing
-// authentication via `authToken`.
+// NewGraphClient creates a new connection to the graph database given by
+// `uri`, performing authentication via `authToken`.
 func NewGraphClient(uri string, authToken AuthToken) (Client, error) {
 	// TODO(mihaimaruseac): Allow configuration to control internal
 	// attributes of the connection (e.g., max connection pool size, etc.)
@@ -59,71 +54,9 @@ func NewGraphClient(uri string, authToken AuthToken) (Client, error) {
 	return driver, nil
 }
 
-// A transaction for the database
+// Transaction is a transaction in the database
 type Transaction = neo4j.Transaction
-// Work done inside a transaction for the database
+// TransactionWork represents work done inside a `Transaction`.
 type TransactionWork = neo4j.TransactionWork
 
 // TODO(mihaimaruseac): Define queries needed for GUAC production
-
-// Runs a simple write query against the graph database.
-// Use only for testing!
-func WriteQueryForTesting(client Client, query string, args map[string]interface{}) error {
-	session := client.NewSession(neo4j.SessionConfig{})
-	defer session.Close()
-
-	_, err := session.WriteTransaction(
-		func(tx Transaction) (interface{}, error) {
-			return tx.Run(query, args)
-		})
-	return err
-}
-
-// Runs a simple read query against the graph database.
-// Use only for testing!
-// Returns the result as an interface to be handled by the caller (as records),
-// but this is not optimal to use in production!
-func ReadQueryForTesting(client Client, query string, args map[string]interface{}) ([][]interface{}, error) { ///*(interface{}, error) { //*/(neo4j.Result, error) {
-	session := client.NewSession(neo4j.SessionConfig{})
-	defer session.Close()
-
-	// For this testing function, we just collect all values.
-	values := make([][]interface{}, 0)
-
-	_, err := session.ReadTransaction(
-		func(tx Transaction) (interface{}, error) {
-			records, err := tx.Run(query, args)
-			if err != nil {
-				return nil, err
-			}
-			// Since `records` is valid only while `tx` is in
-			// scope, we have to process all data here.
-			for records.Next() {
-				record := records.Record()
-				values = append(values, record.Values)
-			}
-			if err = records.Err(); err != nil {
-				return nil, err
-			}
-			return values, err
-		})
-
-	if err != nil {
-		return nil, err
-	}
-	return values, err
-}
-
-// Clears the entire database.
-// Use only for testing!
-// Very slow on large amounts of data!
-func ClearDBForTesting(client Client) error {
-	session := client.NewSession(neo4j.SessionConfig{})
-	defer session.Close()
-
-	_, err := session.WriteTransaction(
-		func(tx Transaction) (interface{}, error) {
-			return tx.Run("MATCH (n) DETACH DELETE n", nil)
-		})
-	return err
-}
