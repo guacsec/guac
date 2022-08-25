4936a262c2d00c879b91e4adccd326aef072ced2
Mihai Maruseac <mihaimaruseac@google.com>
2022-08-25 12:27:56 -0700

    Add back testing utils
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

---
+++ pkg/assembler/graphdb/graphdb_test_utils.go
@@ -0,0 +1,90 @@
+//
+// Copyright 2022 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Note: All this code here is temporary and will change often. This module
+// must be a leaf in the dependency tree!
+
+package graphdb
+
+import (
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+)
+
+// CreateAuthTokenForTesting creates an empty authentication token to be used
+// in testing!
+func CreateAuthTokenForTesting() AuthToken {
+	return neo4j.NoAuth()
+}
+
+// WriteQueryForTesting runs a simple write query against the graph database.
+func WriteQueryForTesting(client Client, query string, args map[string]interface{}) error {
+	session := client.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+
+	_, err := session.WriteTransaction(
+		func(tx Transaction) (interface{}, error) {
+			return tx.Run(query, args)
+		})
+	return err
+}
+
+// ReadQueryForTesting runs a simple read query against the graph database.
+//
+// Returns the result as an interface to be handled by the caller (as records),
+// but this is not optimal to use in production!
+func ReadQueryForTesting(client Client, query string, args map[string]interface{}) ([][]interface{}, error) { ///*(interface{}, error) { //*/(neo4j.Result, error) {
+	session := client.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+
+	// For this testing function, we just collect all values.
+	values := make([][]interface{}, 0)
+
+	_, err := session.ReadTransaction(
+		func(tx Transaction) (interface{}, error) {
+			records, err := tx.Run(query, args)
+			if err != nil {
+				return nil, err
+			}
+			// Since `records` is valid only while `tx` is in
+			// scope, we have to process all data here.
+			for records.Next() {
+				record := records.Record()
+				values = append(values, record.Values)
+			}
+			if err = records.Err(); err != nil {
+				return nil, err
+			}
+			return values, err
+		})
+
+	if err != nil {
+		return nil, err
+	}
+	return values, err
+}
+
+// ClearDBForTesting clears the entire database.
+//
+// It is very slow on large amounts of data!
+func ClearDBForTesting(client Client) error {
+	session := client.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+
+	_, err := session.WriteTransaction(
+		func(tx Transaction) (interface{}, error) {
+			return tx.Run("MATCH (n) DETACH DELETE n", nil)
+		})
+	return err
+}
