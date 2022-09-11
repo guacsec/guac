665484cef1bbf7254296cadc87cf60fe3e25fc55
Michael Lieberman <mlieberman85@gmail.com>
2022-09-11 17:41:51 +0000

    Add file collector
    Signed-off-by: Michael Lieberman <mlieberman85@gmail.com>

---
+++ pkg/handler/collector/file/file.go
@@ -0,0 +1,126 @@
+//
+// Copyright 2022 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package file
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/guacsec/guac/pkg/handler/processor"
+)
+
+const (
+	FileCollector = "FileCollector"
+)
+
+type fileCollector struct {
+	path        string
+	lastChecked time.Time
+	poll        bool
+	interval    time.Duration
+}
+
+func NewFileCollector(ctx context.Context, path string, poll bool, interval time.Duration) *fileCollector {
+	return &fileCollector{
+		path:     path,
+		poll:     poll,
+		interval: interval,
+	}
+}
+
+// RetrieveArtifacts collects the documents from the collector. It emits each collected
+// document through the channel to be collected and processed by the upstream processor.
+// The function should block until all the artifacts are collected and return a nil error
+// or return an error from the collector crashing. This function can keep running and check
+// for new artifacts as they are being uploaded by polling on an interval or run once and
+// grab all the artifacts and end.
+func (f *fileCollector) RetrieveArtifacts(ctx context.Context, docChannel chan<- *processor.Document) error {
+	if _, err := os.Stat(f.path); os.IsExist(err) {
+		return fmt.Errorf("path: %s does not exist", f.path)
+	}
+
+	readFunc := func(path string, dirEntry fs.DirEntry, err error) error {
+		// If the context has been canceled it contains an err which we can throw.
+		// When it gets thrown a second time will cancel the walk.
+		// See filepath.WalkDir for more info.
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+		// NOTE: Explicitly rethrowing new errors if a particular directory has an error.
+		// If we rethrow the error it kills the whole walk. Still useful to make it explicit that we ran into an error.
+		if err != nil {
+			return fmt.Errorf("path: %s is invalid", path)
+		}
+		if dirEntry.IsDir() {
+			return nil
+		}
+		if info, err := dirEntry.Info(); !info.ModTime().After(f.lastChecked) || err != nil {
+			return err
+		}
+
+		blob, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		doc := &processor.Document{
+			Blob:   blob,
+			Type:   processor.DocumentUnknown,
+			Format: processor.FormatUnknown,
+			SourceInformation: processor.SourceInformation{
+				Collector: string(FileCollector),
+				Source:    fmt.Sprintf("file:///%s", path),
+			},
+		}
+
+		docChannel <- doc
+
+		return nil
+	}
+
+	if f.poll {
+		for {
+			err := filepath.WalkDir(f.path, readFunc)
+			if err != nil {
+				if errors.Is(err, ctx.Err()) {
+					return nil
+				}
+				return err
+			}
+			f.lastChecked = time.Now()
+			time.Sleep(f.interval)
+		}
+	} else {
+		err := filepath.WalkDir(f.path, readFunc)
+		if err != nil {
+			return err
+		}
+		f.lastChecked = time.Now()
+	}
+
+	return nil
+}
+
+// Type returns the collector type
+func (f *fileCollector) Type() string {
+	return FileCollector
+}
---
+++ pkg/handler/collector/file/file_test.go
@@ -0,0 +1,143 @@
+//
+// Copyright 2022 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package file
+
+import (
+	"context"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/guacsec/guac/pkg/handler/processor"
+)
+
+func Test_fileCollector_RetrieveArtifacts(t *testing.T) {
+	type fields struct {
+		path        string
+		lastChecked time.Time
+		poll        bool
+		interval    time.Duration
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []*processor.Document
+		wantErr bool
+	}{{
+		name: "nonexistent file path",
+		fields: fields{
+			path:        "./doesnotexist",
+			lastChecked: time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC),
+			poll:        false,
+			interval:    0,
+		},
+		want:    []*processor.Document{},
+		wantErr: true,
+	}, {
+		name: "found file",
+		fields: fields{
+			path:        "./testdata",
+			lastChecked: time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC),
+			poll:        false,
+			interval:    0,
+		},
+		want: []*processor.Document{{
+			Blob:   []byte("hello\n"),
+			Type:   processor.DocumentUnknown,
+			Format: processor.FormatUnknown,
+			SourceInformation: processor.SourceInformation{
+				Collector: string(FileCollector),
+				Source:    "file:///testdata/hello",
+			}},
+		},
+		wantErr: false,
+	}, {
+		name: "with canceled poll",
+		fields: fields{
+			path:        "./testdata",
+			lastChecked: time.Date(2009, 11, 17, 20, 34, 58, 651387237, time.UTC),
+			poll:        true,
+			interval:    time.Millisecond,
+		},
+		want: []*processor.Document{{
+			Blob:   []byte("hello\n"),
+			Type:   processor.DocumentUnknown,
+			Format: processor.FormatUnknown,
+			SourceInformation: processor.SourceInformation{
+				Collector: string(FileCollector),
+				Source:    "file:///testdata/hello",
+			}},
+		},
+		wantErr: false,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &fileCollector{
+				path:        tt.fields.path,
+				lastChecked: tt.fields.lastChecked,
+				poll:        tt.fields.poll,
+				interval:    tt.fields.interval,
+			}
+			// NOTE: Below is one of the simplest ways to validate the context getting canceled()
+			// This is still brittle if a test for some reason takes longer than a second.
+			// With that said, the tests are simple and this should only trigger to cancel polling.
+			var ctx context.Context
+			var cancel context.CancelFunc
+			if f.poll {
+				ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+				defer cancel()
+			} else {
+				ctx = context.Background()
+			}
+			docChan := make(chan *processor.Document, 1)
+			errChan := make(chan error, 1)
+			defer close(docChan)
+			defer close(errChan)
+			go func() {
+				errChan <- f.RetrieveArtifacts(ctx, docChan)
+			}()
+
+			numCollectors := 1
+			collectorsDone := 0
+
+			s := []*processor.Document{}
+
+			for collectorsDone < numCollectors {
+				select {
+				case d := <-docChan:
+					s = append(s, d)
+				case err := <-errChan:
+					if (err != nil) != tt.wantErr {
+						t.Errorf("fileCollector.RetrieveArtifacts() = %v, want %v", err, tt.wantErr)
+					}
+					collectorsDone += 1
+				}
+			}
+
+			// Drain anything in document channel
+			for len(docChan) > 0 {
+				d := <-docChan
+				s = append(s, d)
+			}
+			if !reflect.DeepEqual(s, tt.want) {
+				t.Errorf("fileCollector.RetrieveArtifacts() = %v, want %v", s, tt.want)
+			}
+			if f.Type() != FileCollector {
+				t.Errorf("fileCollector.Type() = %s, want %s", f.Type(), FileCollector)
+			}
+		})
+	}
+}
