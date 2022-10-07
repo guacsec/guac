080bbd8808c7763395387187a51cbd38695ff7f7
Nadgowda, Shripad <shripad.nadgowda@intel.com>
2022-10-07 10:49:02 -0400

    changed node type for image
    Signed-off-by: Nadgowda, Shripad <shripad.nadgowda@intel.com>

--- pkg/ingestor/parser/cyclonedx/parser_cyclonedx.go
+++ pkg/ingestor/parser/cyclonedx/parser_cyclonedx.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	cdx "github.com/CycloneDX/cyclonedx-go"
 	"github.com/guacsec/guac/pkg/assembler"
@@ -11,24 +12,44 @@ import (
 )
 
 type cyclonedxParser struct {
-	subject  assembler.ArtifactNode
-	packages []assembler.PackageNode
+	rootPackage parentPackages
+}
+
+type parentPackages struct {
+	curPackage  assembler.PackageNode
+	depPackages []parentPackages
 }
 
 func NewCycloneDXParser() *cyclonedxParser {
 	return &cyclonedxParser{
-		subject:  assembler.ArtifactNode{},
-		packages: []assembler.PackageNode{},
+		rootPackage: parentPackages{},
 	}
 }
 
 func (c *cyclonedxParser) CreateNodes(ctx context.Context) []assembler.GuacNode {
 	nodes := []assembler.GuacNode{}
-	nodes = append(nodes, c.subject)
-	for _, p := range c.packages {
-		nodes = append(nodes, p)
-	}
+	addNodes(c.rootPackage, &nodes)
 	return nodes
+}
+
+func addNodes(curPkg parentPackages, nodes *[]assembler.GuacNode) {
+	*nodes = append(*nodes, curPkg.curPackage)
+	for _, d := range curPkg.depPackages {
+		addNodes(d, nodes)
+	}
+}
+
+func addEdges(curPkg parentPackages, edges *[]assembler.GuacEdge) {
+	// this could happen if we image purl creation fails for rootPackage
+	// we need better solution to support different image name formats in SBOM
+	if curPkg.curPackage.Name == "" {
+		return
+	}
+	for _, d := range curPkg.depPackages {
+		fmt.Printf("%s-%s\n", d.curPackage.Name, curPkg.curPackage.Name)
+		*edges = append(*edges, assembler.DependsOnEdge{PackageNode: d.curPackage, PackageDependency: curPkg.curPackage})
+		// addEdges(d, edges)
+	}
 }
 
 // Parse breaks out the document into the graph components
@@ -37,7 +58,7 @@ func (c *cyclonedxParser) Parse(ctx context.Context, doc *processor.Document) er
 	if err != nil {
 		return fmt.Errorf("failed to parse cyclonedx BOM: %w", err)
 	}
-	c.addSubject(cdxBom)
+	c.addRootPackage(cdxBom)
 	c.addPackages(cdxBom)
 
 	return nil
@@ -50,25 +71,36 @@ func (c *cyclonedxParser) GetIdentities(ctx context.Context) []assembler.Identit
 
 func (c *cyclonedxParser) CreateEdges(ctx context.Context, foundIdentities []assembler.IdentityNode) []assembler.GuacEdge {
 	edges := []assembler.GuacEdge{}
-
-	for _, p := range c.packages {
-		edges = append(edges, assembler.ContainsEdge{PackageNode: p, ContainedArtifact: c.subject})
-	}
+	addEdges(c.rootPackage, &edges)
 	return edges
 }
 
-func (c *cyclonedxParser) addSubject(cdxBom *cdx.BOM) {
-	c.subject.Name = cdxBom.Metadata.Component.Name
-	c.subject.Digest = cdxBom.Metadata.Component.Version
+func (c *cyclonedxParser) addRootPackage(cdxBom *cdx.BOM) {
+	// oci purl: pkg:oci/debian@sha256%3A244fd47e07d10?repository_url=ghcr.io/debian&tag=bullseye
+	splitImage := strings.Split(cdxBom.Metadata.Component.Name, "/")
+	if len(splitImage) == 3 {
+		rootPackage := assembler.PackageNode{}
+		rootPackage.Purl = "pkg:oci/" + splitImage[2] + "?repository_url=" + splitImage[0] + "/" + splitImage[1]
+		rootPackage.Name = cdxBom.Metadata.Component.Name
+		rootPackage.Digest = append(rootPackage.Digest, cdxBom.Metadata.Component.Version)
+		c.rootPackage = parentPackages{
+			curPackage:  rootPackage,
+			depPackages: []parentPackages{},
+		}
+	}
 }
 
 func (c *cyclonedxParser) addPackages(cdxBom *cdx.BOM) {
 	for _, comp := range *cdxBom.Components {
-		c.packages = append(c.packages, assembler.PackageNode{
+		curPkg := assembler.PackageNode{
 			Name:   comp.Name,
 			Digest: []string{comp.Version},
 			Purl:   comp.PackageURL,
 			CPEs:   []string{comp.CPE},
+		}
+		c.rootPackage.depPackages = append(c.rootPackage.depPackages, parentPackages{
+			curPackage:  curPkg,
+			depPackages: []parentPackages{},
 		})
 	}
 }
