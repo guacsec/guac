5e3c1d51c576523a37837a7801224f3b48ad16e3
cpendery <cpendery@vt.edu>
2022-10-12 22:50:46 -0400

    feat: add xml support for cyclonedx guesser
    Signed-off-by: cpendery <cpendery@vt.edu>

--- pkg/handler/processor/guesser/test_cyclonedx_test.go
+++ pkg/handler/processor/guesser/test_cyclonedx_test.go
@@ -26,34 +26,50 @@ func Test_cyclonedxTypeGuesser_GuessDocumentType(t *testing.T) {
 	testCases := []struct {
 		name     string
 		blob     []byte
+		format   processor.FormatType
 		expected processor.DocumentType
 	}{{
 		name: "invalid cyclonedx Document",
 		blob: []byte(`{
 			"abc": "def"
 		}`),
+		format:   processor.FormatJSON,
 		expected: processor.DocumentUnknown,
 	}, {
 		name:     "invalid cyclonedx Document",
 		blob:     testdata.CycloneDXInvalidExample,
+		format:   processor.FormatJSON,
+		expected: processor.DocumentUnknown,
+	}, {
+		name:     "invalid xml cyclonedx Document",
+		blob:     testdata.CycloneDXInvalidExampleXML,
+		format:   processor.FormatXML,
 		expected: processor.DocumentUnknown,
 	}, {
 		name:     "valid small cyclonedx Document",
 		blob:     testdata.CycloneDXBusyboxExample,
+		format:   processor.FormatJSON,
 		expected: processor.DocumentCycloneDX,
 	}, {
 		name:     "valid distroless cyclonedx Document",
 		blob:     testdata.CycloneDXDistrolessExample,
+		format:   processor.FormatJSON,
 		expected: processor.DocumentCycloneDX,
 	}, {
 		name:     "valid alpine cyclonedx Document",
 		blob:     testdata.CycloneDXExampleAlpine,
+		format:   processor.FormatJSON,
+		expected: processor.DocumentCycloneDX,
+	}, {
+		name:     "valid xml cyclonedx Document",
+		blob:     testdata.CycloneDXExampleLaravelXML,
+		format:   processor.FormatXML,
 		expected: processor.DocumentCycloneDX,
 	}}
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
 			guesser := &cycloneDXTypeGuesser{}
-			f := guesser.GuessDocumentType(tt.blob, processor.FormatJSON)
+			f := guesser.GuessDocumentType(tt.blob, tt.format)
 			if f != tt.expected {
 				t.Errorf("got the wrong format, got %v, expected %v", f, tt.expected)
 			}
