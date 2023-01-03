46f00760a8e8d5713b9475ffe0f74e3b46847e4b
Parth Patel <88045217+pxp928@users.noreply.github.com>
2023-01-03 13:11:55 -0500

    added NATS with Jet Stream enabled (NATS PR: 1 of 4) (#255)
    * added nats_emitter with jetstream enabled
    
    Signed-off-by: pxp928 <parth.psu@gmail.com>
    
    * updates based on comments and added Subscribe test to nats emitter
    
    Signed-off-by: pxp928 <parth.psu@gmail.com>
    
    * moved processor testdata to remove import cycle in emitter
    
    Signed-off-by: pxp928 <parth.psu@gmail.com>
    
    * removed sleep from nats test server as not needed
    
    Signed-off-by: pxp928 <parth.psu@gmail.com>
    
    * removed dependency on process for nats emitter test
    
    Signed-off-by: pxp928 <parth.psu@gmail.com>
    
    * updated error messages and added another test for recreateStream
    
    Signed-off-by: pxp928 <parth.psu@gmail.com>
    
    Signed-off-by: pxp928 <parth.psu@gmail.com>

---
+++ pkg/emitter/nats_emitter.go
@@ -0,0 +1,160 @@
+//
+// Copyright 2022 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package emitter
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/guacsec/guac/pkg/logging"
+	"github.com/nats-io/nats.go"
+)
+
+// NATS stream
+const (
+	NatsName                string = "GUAC"
+	StreamName              string = "DOCUMENTS"
+	StreamSubjects          string = "DOCUMENTS.*"
+	SubjectNameDocCollected string = "DOCUMENTS.collected"
+	SubjectNameDocProcessed string = "DOCUMENTS.processed"
+	SubjectNameDocParsed    string = "DOCUMENTS.parsed"
+)
+
+type jetStream struct {
+	// url of the NATS server to connect to
+	url string
+	// creds is the user credentials file for NATS authentication
+	// either user credentials or NKey needs to be specified
+	creds string
+	// nKeyFile is the alternative method of login for NATS
+	// either user credentials or NKey needs to be specified
+	nKeyFile string
+	// nc is the NATS connection
+	nc *nats.Conn
+	// js is the context to the jetstream once initialized on NATS
+	js nats.JetStreamContext
+}
+
+// NewJetStream initializes jetStream to connect to NATS
+func NewJetStream(url string, creds string, nKeyFile string) *jetStream {
+	return &jetStream{
+		url:      url,
+		creds:    creds,
+		nKeyFile: nKeyFile,
+	}
+}
+
+// JetStreamInit initializes NATS and enabled Jet Stream to be used for GUAC
+func (j *jetStream) JetStreamInit(ctx context.Context) (context.Context, error) {
+	var err error
+	// Connect Options.
+	opts := []nats.Option{nats.Name(NatsName)}
+
+	// Use UserCredentials
+	if j.creds != "" {
+		opts = append(opts, nats.UserCredentials(j.creds))
+	}
+
+	// Use Nkey authentication.
+	if j.nKeyFile != "" {
+		opt, err := nats.NkeyOptionFromSeed(j.nKeyFile)
+		if err != nil {
+			return ctx, fmt.Errorf("failed to load nKeyFile for nats: %w", err)
+		}
+		opts = append(opts, opt)
+	}
+
+	// Connect to NATS
+	nc, err := nats.Connect(j.url, opts...)
+	if err != nil {
+		return ctx, fmt.Errorf("unable to connect to nats server: %w", err)
+	}
+	// Create JetStream Context
+	js, err := nc.JetStream()
+
+	if err != nil {
+		nc.Close()
+		return ctx, fmt.Errorf("unable to connect to nats jetstream: %w", err)
+	}
+	err = createStreamOrExists(ctx, js)
+	if err != nil {
+		nc.Close()
+		return ctx, fmt.Errorf("failed to create stream: %w", err)
+	}
+
+	j.nc = nc
+	j.js = js
+
+	return withJetstream(ctx, js), nil
+}
+
+func createStreamOrExists(ctx context.Context, js nats.JetStreamContext) error {
+	logger := logging.FromContext(ctx)
+	_, err := js.StreamInfo(StreamName)
+
+	if err != nil && !errors.Is(err, nats.ErrStreamNotFound) {
+		return err
+	}
+	// stream not found, create it
+	if errors.Is(err, nats.ErrStreamNotFound) {
+		logger.Infof("creating stream %q and subjects %q", StreamName, StreamSubjects)
+		_, err = js.AddStream(&nats.StreamConfig{
+			Name:      StreamName,
+			Subjects:  []string{StreamSubjects},
+			Retention: nats.WorkQueuePolicy,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Close closes the NATS connection
+func (j *jetStream) Close() {
+	if j.nc != nil {
+		j.nc.Close()
+	}
+}
+
+// RecreateStream deletes the current existing stream and recreates it
+func (j *jetStream) RecreateStream(ctx context.Context) error {
+	if j.js != nil {
+		err := j.js.DeleteStream(StreamName)
+		if err != nil && !errors.Is(err, nats.ErrStreamNotFound) {
+			return fmt.Errorf("failed to delete stream: %w", err)
+		}
+	}
+	err := createStreamOrExists(ctx, j.js)
+	if err != nil {
+		j.Close()
+		return fmt.Errorf("failed to create stream: %w", err)
+	}
+	return nil
+}
+
+func withJetstream(ctx context.Context, js nats.JetStreamContext) context.Context {
+	return context.WithValue(ctx, jetStream{}, js)
+}
+
+// FromContext allows for the JetStreamContext to be pulled from the context
+func FromContext(ctx context.Context) nats.JetStreamContext {
+	if js, ok := ctx.Value(jetStream{}).(nats.JetStreamContext); ok {
+		return js
+	}
+	return nil
+}
---
+++ pkg/emitter/nats_emitter_test.go
@@ -0,0 +1,256 @@
+//
+// Copyright 2022 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package emitter
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/guacsec/guac/internal/testing/dochelper"
+	nats_test "github.com/guacsec/guac/internal/testing/nats"
+	"github.com/guacsec/guac/pkg/handler/processor"
+	"github.com/guacsec/guac/pkg/logging"
+	"github.com/nats-io/nats.go"
+	uuid "github.com/satori/go.uuid"
+)
+
+var (
+	// Taken from: https://slsa.dev/provenance/v0.1#example
+	ite6SLSA = `
+	{
+		"_type": "https://in-toto.io/Statement/v0.1",
+		"subject": [{"name": "helloworld", "digest": {"sha256": "5678..."}}],
+		"predicateType": "https://slsa.dev/provenance/v0.2",
+		"predicate": {
+			"builder": { "id": "https://github.com/Attestations/GitHubHostedActions@v1" },
+			"buildType": "https://github.com/Attestations/GitHubActionsWorkflow@v1",
+			"invocation": {
+			  "configSource": {
+				"uri": "git+https://github.com/curl/curl-docker@master",
+				"digest": { "sha1": "d6525c840a62b398424a78d792f457477135d0cf" },   
+				"entryPoint": "build.yaml:maketgz"
+			  }
+			},
+			"metadata": {
+			  "buildStartedOn": "2020-08-19T08:38:00Z",
+			  "completeness": {
+				  "environment": true
+			  }
+			},
+			"materials": [
+			  {
+				"uri": "git+https://github.com/curl/curl-docker@master",
+				"digest": { "sha1": "d6525c840a62b398424a78d792f457477135d0cf" }
+			  }, {
+				"uri": "github_hosted_vm:ubuntu-18.04:20210123.1",
+				"digest": { "sha1": "d6525c840a62b398424a78d792f457477135d0cf" }
+			  }
+			]
+		}
+	}`
+
+	ite6SLSADoc = processor.Document{
+		Blob:   []byte(ite6SLSA),
+		Type:   processor.DocumentITE6SLSA,
+		Format: processor.FormatJSON,
+		SourceInformation: processor.SourceInformation{
+			Collector: "TestCollector",
+			Source:    "TestSource",
+		},
+	}
+)
+
+func TestNatsEmitter_PublishOnEmit(t *testing.T) {
+	expectedDocTree := dochelper.DocNode(&ite6SLSADoc)
+
+	natsTest := nats_test.NewNatsTestServer()
+	url, err := natsTest.EnableJetStreamForTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer natsTest.Shutdown()
+
+	ctx := context.Background()
+	jetStream := NewJetStream(url, "", "")
+	ctx, err = jetStream.JetStreamInit(ctx)
+	if err != nil {
+		t.Fatalf("unexpected error initializing jetstream: %v", err)
+	}
+	err = jetStream.RecreateStream(ctx)
+	if err != nil {
+		t.Fatalf("unexpected error recreating jetstream: %v", err)
+	}
+	defer jetStream.Close()
+	err = testPublish(ctx, &ite6SLSADoc)
+	if err != nil {
+		t.Fatalf("unexpected error on emit: %v", err)
+	}
+
+	var cancel context.CancelFunc
+
+	ctx, cancel = context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
+	docChan := make(chan processor.DocumentTree, 1)
+	errChan := make(chan error, 1)
+	defer close(errChan)
+	go func() {
+		errChan <- testSubscribe(ctx, docChan)
+	}()
+
+	numSubscribers := 1
+	subscribersDone := 0
+
+	for subscribersDone < numSubscribers {
+		select {
+		case d := <-docChan:
+			if !dochelper.DocTreeEqual(d, expectedDocTree) {
+				t.Errorf("doc tree did not match up, got\n%s, \nexpected\n%s", dochelper.StringTree(d), dochelper.StringTree(expectedDocTree))
+			}
+		case err := <-errChan:
+			if err != nil && !errors.Is(err, context.DeadlineExceeded) {
+				t.Errorf("nats emitter Subscribe test errored = %v", err)
+			}
+			subscribersDone += 1
+		}
+	}
+
+}
+
+func TestNatsEmitter_RecreateStream(t *testing.T) {
+	natsTest := nats_test.NewNatsTestServer()
+	url, err := natsTest.EnableJetStreamForTest()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer natsTest.Shutdown()
+
+	ctx := context.Background()
+	jetStream := NewJetStream(url, "", "")
+	ctx, err = jetStream.JetStreamInit(ctx)
+	if err != nil {
+		t.Fatalf("unexpected error initializing jetstream: %v", err)
+	}
+	defer jetStream.Close()
+	tests := []struct {
+		name           string
+		deleteStream   bool
+		wantErrMessage error
+	}{{
+		name:           "no new stream",
+		deleteStream:   false,
+		wantErrMessage: nats.ErrStreamNotFound,
+	}, {
+		name:           "delete stream and recreate",
+		deleteStream:   true,
+		wantErrMessage: nats.ErrStreamNotFound,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.deleteStream {
+				err := jetStream.js.DeleteStream(StreamName)
+				if err != nil {
+					t.Errorf("failed to delete stream: %v", err)
+				}
+				_, err = jetStream.js.StreamInfo(StreamName)
+				if err == nil || (err != nil) && !errors.Is(err, tt.wantErrMessage) {
+					t.Errorf("RecreateStream() error = %v, wantErr %v", err, tt.wantErrMessage)
+					return
+				}
+			}
+			err = jetStream.RecreateStream(ctx)
+			if err != nil {
+				t.Fatalf("unexpected error recreating jetstream: %v", err)
+			}
+			_, err = jetStream.js.StreamInfo(StreamName)
+			if err != nil {
+				t.Errorf("RecreateStream() failed to create stream with error = %v", err)
+				return
+			}
+		})
+	}
+}
+
+func testPublish(ctx context.Context, d *processor.Document) error {
+	logger := logging.FromContext(ctx)
+	js := FromContext(ctx)
+	docByte, err := json.Marshal(d)
+	if err != nil {
+		return fmt.Errorf("failed marshal of document: %w", err)
+	}
+	_, err = js.Publish(SubjectNameDocCollected, docByte)
+	if err != nil {
+		return fmt.Errorf("failed to publish document on stream: %w", err)
+	}
+	logger.Infof("doc published: %+v", d)
+	return nil
+}
+
+func testSubscribe(ctx context.Context, docChannel chan<- processor.DocumentTree) error {
+	logger := logging.FromContext(ctx)
+	js := FromContext(ctx)
+	id := uuid.NewV4().String()
+	sub, err := js.PullSubscribe(SubjectNameDocCollected, "processor")
+	if err != nil {
+		logger.Errorf("processor subscribe failed: %s", err)
+		return err
+	}
+	for {
+		// if the context is canceled we want to break out of the loop
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+		msgs, err := sub.Fetch(1)
+		if err != nil {
+			if errors.Is(err, nats.ErrTimeout) {
+				logger.Infof("[processor: %s] error consuming, backing off for a second: %v", id, err)
+				time.Sleep(1 * time.Second)
+				continue
+			} else {
+				return err
+			}
+		}
+		if len(msgs) > 0 {
+			err := msgs[0].Ack()
+			if err != nil {
+				logger.Errorf("[processor: %v] unable to Ack: %v", id, err)
+				return err
+			}
+			doc := processor.Document{}
+			err = json.Unmarshal(msgs[0].Data, &doc)
+			if err != nil {
+				logger.Warnf("[processor: %s] failed unmarshal the document bytes: %v", id, err)
+			}
+
+			docNode := &processor.DocumentNode{
+				Document: &doc,
+				Children: nil,
+			}
+
+			docTree := processor.DocumentTree(docNode)
+
+			logger.Infof("[processor: %s] docTree Processed: %+v", id, docTree.Document.SourceInformation)
+			if err != nil {
+				return err
+			}
+			docChannel <- docTree
+		}
+	}
+}
