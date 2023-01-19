70e6f1f3ca30b4d6050948fdf3882eec263c4f73
Mihai Maruseac <mihaimaruseac@google.com>
2023-01-18 16:35:40 -0800

    Add files needed for the generation
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

---
+++ pkg/assembler/graphql/graphql.go
@@ -0,0 +1,6 @@
+package graphql
+
+//go:generate go run github.com/99designs/gqlgen generate
+
+// This file is currently empty and used only to allow generating the schema
+// by executing `go generate ./...`.
