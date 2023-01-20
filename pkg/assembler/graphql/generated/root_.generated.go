39c5f4fe23297e3c4ce036c82611db89d2efcccb
Mihai Maruseac <mihaimaruseac@google.com>
2023-01-19 17:27:50 -0800

    Add license header
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- pkg/assembler/graphql/generated/root_.generated.go
+++ pkg/assembler/graphql/generated/root_.generated.go
@@ -671,7 +671,22 @@ func (ec *executionContext) introspectType(name string) (*introspection.Type, er
 }
 
 var sources = []*ast.Source{
-	{Name: "../schema.graphql", Input: `# NOTE: This is experimental and will change in the near future.
+	{Name: "../schema.graphql", Input: `#
+# Copyright 2023 The GUAC Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# NOTE: This is experimental and will change in the near future.
 
 """
 NodeInfo contains fields that are common for any GUAC node. These are metadata
