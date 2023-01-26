1ec9833d4d9918080f86198f4c848397c8cd99a8
pxp928 <parth.psu@gmail.com>
2023-01-26 14:32:33 -0500

    updated cdx purl parsing to match spec
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- pkg/ingestor/parser/cyclonedx/parser_cyclonedx.go
+++ pkg/ingestor/parser/cyclonedx/parser_cyclonedx.go
@@ -113,10 +113,25 @@ func (c *cyclonedxParser) addRootPackage(cdxBom *cdx.BOM) {
 		} else {
 			splitImage := strings.Split(cdxBom.Metadata.Component.Name, "/")
 			if len(splitImage) == 3 {
-				rootPackage.Purl = "pkg:oci/" + splitImage[2] + "?repository_url=" + splitImage[0] + "/" + splitImage[1]
-				rootPackage.Version = cdxBom.Metadata.Component.Version
-				rootPackage.Digest = append(rootPackage.Digest, cdxBom.Metadata.Component.Version)
-				rootPackage.Tags = []string{"CONTAINER"}
+				// example: gcr.io/distroless/static:nonroot
+				splitTag := strings.Split(splitImage[2], ":")
+				if len(splitTag) == 2 {
+					rootPackage.Purl = "pkg:oci/" + splitTag[0] + "@" + cdxBom.Metadata.Component.Version +
+						"?repository_url=" + splitImage[0] + "/" + splitImage[1] + "/" + splitTag[0] + "&tag=" + splitTag[1]
+					rootPackage.Version = cdxBom.Metadata.Component.Version
+					rootPackage.Digest = append(rootPackage.Digest, cdxBom.Metadata.Component.Version)
+					rootPackage.Tags = []string{"CONTAINER"}
+				}
+			} else if len(splitImage) == 2 {
+				// example: library/debian:latest
+				splitTag := strings.Split(splitImage[1], ":")
+				if len(splitTag) == 2 {
+					rootPackage.Purl = "pkg:oci/" + splitTag[0] + "@" + cdxBom.Metadata.Component.Version +
+						"?repository_url=" + splitImage[0] + "/" + splitTag[0] + "&tag=" + splitTag[1]
+					rootPackage.Version = cdxBom.Metadata.Component.Version
+					rootPackage.Digest = append(rootPackage.Digest, cdxBom.Metadata.Component.Version)
+					rootPackage.Tags = []string{"CONTAINER"}
+				}
 			}
 		}
 		c.rootComponent = component{
