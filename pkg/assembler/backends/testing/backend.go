9f45057b9542d0bf805e2880bbffd3884b5186d0
Mihai Maruseac <mihaimaruseac@google.com>
2023-01-31 19:38:30 -0800

    Implement resolver for package trie
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- pkg/assembler/backends/testing/backend.go
+++ pkg/assembler/backends/testing/backend.go
@@ -98,3 +98,52 @@ func filterVersion(n *model.PackageName, pkgSpec *model.PkgSpec) *model.PackageN
 		Versions: versions,
 	}
 }
+
+func (c *demoClient) Packages(ctx context.Context, pkgSpec *model.PkgSpec) ([]*model.Package, error) {
+	var packages []*model.Package
+	for _, p := range demo.packages {
+		if pkgSpec.Type == nil || p.Type == *pkgSpec.Type {
+			packages = append(packages, filterNamespace(p, pkgSpec))
+		}
+	}
+	return packages, nil
+}
+
+func filterNamespace(pkg *model.Package, pkgSpec *model.PkgSpec) *model.Package {
+	var namespaces []*model.PackageNamespace
+	for _, ns := range pkg.Namespaces {
+		if pkgSpec.Namespace == nil || ns.Namespace == *pkgSpec.Namespace {
+			namespaces = append(namespaces, filterName(ns, pkgSpec))
+		}
+	}
+	return &model.Package{
+		Type:       pkg.Type,
+		Namespaces: namespaces,
+	}
+}
+
+func filterName(ns *model.PackageNamespace, pkgSpec *model.PkgSpec) *model.PackageNamespace {
+	var names []*model.PackageName
+	for _, n := range ns.Names {
+		if pkgSpec.Name == nil || n.Name == *pkgSpec.Name {
+			names = append(names, filterVersion(n, pkgSpec))
+		}
+	}
+	return &model.PackageNamespace{
+		Namespace: ns.Namespace,
+		Names:     names,
+	}
+}
+
+func filterVersion(n *model.PackageName, pkgSpec *model.PkgSpec) *model.PackageName {
+	var versions []*model.PackageVersion
+	for _, v := range n.Versions {
+		if pkgSpec.Version == nil || v.Version == *pkgSpec.Version {
+			versions = append(versions, v)
+		}
+	}
+	return &model.PackageName{
+		Name:     n.Name,
+		Versions: versions,
+	}
+}
