4352cdaf40df92763f8e4dcfcc1dff6ef097a1a7
Mihai Maruseac <mihaimaruseac@google.com>
2023-01-31 19:36:48 -0800

    New types from new GraphQL schema
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- pkg/assembler/graphql/model/nodes.go
+++ pkg/assembler/graphql/model/nodes.go
@@ -196,6 +196,76 @@ func (this OldPackage) GetCollectorInfo() *string { return this.CollectorInfo }
 
 func (OldPackage) IsArtifactOrPackage() {}
 
+// Package represents a package.
+//
+// In the pURL representation, each Package matches a `pkg:<type>` partial pURL.
+// The `type` field matches the pURL types but we might also use `"guac"` for the
+// cases where the pURL representation is not complete or when we have custom
+// rules.
+//
+// This node is a singleton: backends guarantee that there is exactly one node with
+// the same `type` value.
+//
+// Also note that this is named `Package`, not `PackageType`. This is only to make
+// queries more readable.
+type Package struct {
+	Type       string              `json:"type"`
+	Namespaces []*PackageNamespace `json:"namespaces"`
+}
+
+// PackageName is a name for packages.
+//
+// In the pURL representation, each PackageName matches the
+// `pgk:<type>/<namespace>/<name>` partial pURL.
+//
+// Names are always mandatory.
+//
+// This is the first node in the trie that can be referred to by other parts of
+// GUAC.
+type PackageName struct {
+	Name     string            `json:"name"`
+	Versions []*PackageVersion `json:"versions"`
+}
+
+// PackageNamespace is a namespace for packages.
+//
+// In the pURL representation, each PackageNamespace matches the
+// `pgk:<type>/<namespace>/` partial pURL.
+//
+// Namespaces are optional and type specific. Because they are optional, we use
+// empty string to denote missing namespaces.
+type PackageNamespace struct {
+	Namespace string         `json:"namespace"`
+	Names     []*PackageName `json:"names"`
+}
+
+// PackageVersion is a package version.
+//
+// In the pURL representation, each PackageName matches the
+// `pgk:<type>/<namespace>/<name>@<version>` partial pURL.
+//
+// Versions are optional and each Package type defines own rules for handling them.
+// For this level of GUAC, these are just opaque strings.
+//
+// This node can be referred to by other parts of GUAC.
+type PackageVersion struct {
+	Version string `json:"version"`
+}
+
+// PkgSpec allows filtering the list of packages to return.
+//
+// Each field matches a qualifier from pURL. Use `null` to match on all values at
+// that level. For example, to get all packages in GUAC backend, use a PkgSpec
+// where every field is `null`.
+//
+// Empty string at a field means matching with the empty string.
+type PkgSpec struct {
+	Type      *string `json:"type"`
+	Namespace *string `json:"namespace"`
+	Name      *string `json:"name"`
+	Version   *string `json:"version"`
+}
+
 // ScorecardPayload are payloads of Scorecards metadata.
 type ScorecardPayload struct {
 	Repo             string  `json:"repo"`
