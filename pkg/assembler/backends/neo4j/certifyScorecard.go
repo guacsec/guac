8d70cfc0330351b424a10d53f986588033da87c3
Mihai Maruseac <mihaimaruseac@google.com>
2023-02-28 15:05:47 -0800

    Split `CertifyScorecard`, separating subject from object.
    This is because the ingestion will require these to be separate,
    creating the object only when certification exists.
    
    For now, I did not do the same split for the input spec, but it should
    be doable if we want a consistent interface, though it will make the
    query JSON nest one extra level.
    
    Signed-off-by: Mihai Maruseac <mihaimaruseac@google.com>

--- pkg/assembler/backends/neo4j/certifyScorecard.go
+++ pkg/assembler/backends/neo4j/certifyScorecard.go
@@ -103,7 +103,7 @@ func (c *neo4jClient) Scorecards(ctx context.Context, certifyScorecardSpec *mode
 					return nil, err
 				}
 
-				scorecard := model.Scorecard{
+				scorecard := model.Scorecard {
 					TimeScanned:      certifyScorecardNode.Props[timeScanned].(string),
 					AggregateScore:   certifyScorecardNode.Props[aggregateScore].(float64),
 					Checks:           checks,
@@ -113,7 +113,7 @@ func (c *neo4jClient) Scorecards(ctx context.Context, certifyScorecardSpec *mode
 					Collector:        certifyScorecardNode.Props[collector].(string),
 				}
 
-				certifyScorecard := &model.CertifyScorecard{
+				certifyScorecard := &model.CertifyScorecard {
 					Source:    &src,
 					Scorecard: &scorecard,
 				}
