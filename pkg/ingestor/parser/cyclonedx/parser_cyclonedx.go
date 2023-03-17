fbf05bfc1a0d17de8109d1739e14adf4cef4aec1
Dejan Bosanac <dbosanac@redhat.com>
2023-03-17 11:42:53 +0100

    Clean the old model and support boms without root purl
    Signed-off-by: Dejan Bosanac <dbosanac@redhat.com>

--- pkg/ingestor/parser/cyclonedx/parser_cyclonedx.go
+++ pkg/ingestor/parser/cyclonedx/parser_cyclonedx.go
@@ -32,54 +32,14 @@ import (
 
 type cyclonedxParser struct {
 	doc             *processor.Document
-	rootComponent   component
-	pkgMap          map[string]*component
 	packagePackages map[string][]model.PkgInputSpec
 
 	cdxBom *cdx.BOM
 }
 
-type component struct {
-	curPackage  assembler.PackageNode
-	depPackages []*component
-}
-
 func NewCycloneDXParser() common.DocumentParser {
 	return &cyclonedxParser{
-		rootComponent:   component{},
-		pkgMap:          map[string]*component{},
 		packagePackages: map[string][]model.PkgInputSpec{},
-	}
-}
-
-// TODO(bulldozer): replace with GetPredicates
-// func (c *cyclonedxParser) CreateNodes(ctx context.Context) []assembler.GuacNode {
-// 	nodes := []assembler.GuacNode{}
-// 	nodes = append(nodes, c.rootComponent.curPackage)
-// 	for _, p := range c.rootComponent.depPackages {
-// 		nodes = append(nodes, p.curPackage)
-// 	}
-// 	return nodes
-// }
-
-func addEdges(curPkg component, edges *[]assembler.GuacEdge, visited map[string]bool) {
-	// this could happen if we image purl creation fails for rootPackage
-	// we need better solution to support different image name formats in SBOM
-	if curPkg.curPackage.Name == "" {
-		return
-	}
-	// Exit the function if the package has already been visited
-	if visited[curPkg.curPackage.Name] {
-		return
-	}
-	visited[curPkg.curPackage.Name] = true
-
-	for _, dep := range curPkg.depPackages {
-		// Append the dependency edge to the edges slice
-		*edges = append(*edges, assembler.DependsOnEdge{PackageNode: curPkg.curPackage, PackageDependency: dep.curPackage})
-
-		// Recursively call addEdges for each dependent package
-		addEdges(*dep, edges, visited)
 	}
 }
 
@@ -102,64 +62,48 @@ func (c *cyclonedxParser) GetIdentities(ctx context.Context) []common.TrustInfor
 	return nil
 }
 
-// TODO(bulldozer): replace with GetPredicates
-// func (c *cyclonedxParser) CreateEdges(ctx context.Context, foundIdentities []common.TrustInformation) []assembler.GuacEdge {
-// 	edges := []assembler.GuacEdge{}
-// 	visited := make(map[string]bool)
-// 	addEdges(c.rootComponent, &edges, visited)
-// 	return edges
-// }
-
 func (c *cyclonedxParser) addRootPackage(cdxBom *cdx.BOM) {
-	// oci purl: pkg:oci/debian@sha256%3A244fd47e07d10?repository_url=ghcr.io/debian&tag=bullseye
 	if cdxBom.Metadata.Component != nil {
 		rootPackage := assembler.PackageNode{}
 		rootPackage.Name = cdxBom.Metadata.Component.Name
 		rootPackage.NodeData = *assembler.NewObjectMetadata(c.doc.SourceInformation)
-		if cdxBom.Metadata.Component.PackageURL != "" {
-			topPackage, err := asmhelpers.PurlToPkg(cdxBom.Metadata.Component.PackageURL)
-			if err != nil {
-				return
-			}
-			c.packagePackages[string(cdxBom.Metadata.Component.BOMRef)] = append(c.packagePackages[string(cdxBom.Metadata.Component.BOMRef)], *topPackage)
-		} else {
+		purl := cdxBom.Metadata.Component.PackageURL
+		if cdxBom.Metadata.Component.PackageURL == "" {
 			if cdxBom.Metadata.Component.Type == cdx.ComponentTypeContainer {
 				splitImage := strings.Split(cdxBom.Metadata.Component.Name, "/")
 				if len(splitImage) == 3 {
 					// example: gcr.io/distroless/static:nonroot
 					splitTag := strings.Split(splitImage[2], ":")
 					if len(splitTag) == 2 {
-						rootPackage.Purl = "pkg:oci/" + splitTag[0] + "@" + cdxBom.Metadata.Component.Version +
+						purl = "pkg:oci/" + splitTag[0] + "@" + cdxBom.Metadata.Component.Version +
 							"?repository_url=" + splitImage[0] + "/" + splitImage[1] + "/" + splitTag[0] + "&tag=" + splitTag[1]
 					} else {
 						// no tag specified
-						rootPackage.Purl = "pkg:oci/" + splitImage[2] + "@" + cdxBom.Metadata.Component.Version +
+						purl = "pkg:oci/" + splitImage[2] + "@" + cdxBom.Metadata.Component.Version +
 							"?repository_url=" + splitImage[0] + "/" + splitImage[1] + "/" + splitImage[2] + "&tag="
 					}
 				} else if len(splitImage) == 2 {
 					// example: library/debian:latest
 					splitTag := strings.Split(splitImage[1], ":")
 					if len(splitTag) == 2 {
-						rootPackage.Purl = "pkg:oci/" + splitTag[0] + "@" + cdxBom.Metadata.Component.Version +
+						purl = "pkg:oci/" + splitTag[0] + "@" + cdxBom.Metadata.Component.Version +
 							"?repository_url=" + splitImage[0] + "/" + splitTag[0] + "&tag=" + splitTag[1]
 					} else {
 						// no tag specified
-						rootPackage.Purl = "pkg:oci/" + splitImage[1] + "@" + cdxBom.Metadata.Component.Version +
+						purl = "pkg:oci/" + splitImage[1] + "@" + cdxBom.Metadata.Component.Version +
 							"?repository_url=" + splitImage[0] + "/" + splitImage[1] + "&tag="
 					}
 				}
 			} else if cdxBom.Metadata.Component.Type == cdx.ComponentTypeFile {
 				// example: file type ("/home/work/test/build/webserver/")
-				rootPackage.Purl = "pkg:guac/file/" + cdxBom.Metadata.Component.Name + "&checksum=" + cdxBom.Metadata.Component.Version
+				purl = "pkg:guac/file/" + cdxBom.Metadata.Component.Name + "&checksum=" + cdxBom.Metadata.Component.Version
 			}
-			rootPackage.Version = cdxBom.Metadata.Component.Version
-			rootPackage.Digest = append(rootPackage.Digest, cdxBom.Metadata.Component.Version)
-			rootPackage.Tags = []string{string(cdxBom.Metadata.Component.Type)}
 		}
-		c.rootComponent = component{
-			curPackage:  rootPackage,
-			depPackages: []*component{},
+		topPackage, err := asmhelpers.PurlToPkg(purl)
+		if err != nil {
+			return
 		}
+		c.packagePackages[string(cdxBom.Metadata.Component.BOMRef)] = append(c.packagePackages[string(cdxBom.Metadata.Component.BOMRef)], *topPackage)
 	}
 }
 
