0d62bc44b3a69f982c657e95de8ed9df1521e799
Rebecca Metzman <rmetzman@google.com>
2023-06-22 16:14:54 +0000

    simplifying tests
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>

--- pkg/guacanalytics/pathPlanning_test.go
+++ pkg/guacanalytics/pathPlanning_test.go
@@ -22,237 +22,214 @@ import (
 	"testing"
 	"time"
 
+	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/Khan/genqlient/graphql"
 	"github.com/google/go-cmp/cmp"
-	model "github.com/guacsec/guac/pkg/assembler/clients/generated"
-	"go.uber.org/zap"
-
-	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/guacsec/guac/pkg/assembler"
 	"github.com/guacsec/guac/pkg/assembler/backends/inmem"
+	model "github.com/guacsec/guac/pkg/assembler/clients/generated"
 	"github.com/guacsec/guac/pkg/assembler/graphql/generated"
 	"github.com/guacsec/guac/pkg/assembler/graphql/resolvers"
 	"github.com/guacsec/guac/pkg/logging"
+	"go.uber.org/zap"
 )
 
-const (
-	inmems = "inmem"
-)
-
-func ingestTestData(ctx context.Context, client graphql.Client) {
-	logger := logging.FromContext(ctx)
-
-	topns := "topns"
-	topns1 := "topns1"
-	topns2 := "topns2"
-	topns3 := "topns3"
-	ns := "ubuntu"
-	version := "1.19.0"
-	depns := "openssl.org"
-	depns2 := "openssl.org2"
-	opensslVersion := "3.0.3"
-
-	ingestDependencies := []struct {
-		topTopPkg1           model.PkgInputSpec
-		topTopPkg2           model.PkgInputSpec
-		topTopPkg3           model.PkgInputSpec
-		topPkg               model.PkgInputSpec
-		pkg                  model.PkgInputSpec
-		depPkg1              model.PkgInputSpec
-		depPkg2              model.PkgInputSpec
-		dependencyDirect     model.IsDependencyInputSpec
-		dependencyNotInRange model.IsDependencyInputSpec
-		dependencyIndirect   model.IsDependencyInputSpec
-	}{{
-		topTopPkg1: model.PkgInputSpec{
-			Type:      "top1",
-			Namespace: &topns1,
-			Name:      "toppkg1",
-			Version:   &version,
-		},
-		topTopPkg2: model.PkgInputSpec{
-			Type:      "top2",
-			Namespace: &topns2,
-			Name:      "toppkg2",
-			Version:   &version,
-		},
-		topTopPkg3: model.PkgInputSpec{
-			Type:      "top3",
-			Namespace: &topns3,
-			Name:      "toppkg3",
-			Version:   &version,
-		},
-		topPkg: model.PkgInputSpec{
-			Type:      "top",
-			Namespace: &topns,
-			Name:      "toppkg",
-			Version:   &version,
-		},
-		pkg: model.PkgInputSpec{
-			Type:      "deb",
-			Namespace: &ns,
-			Name:      "dpkg",
-			Version:   &version,
-			Qualifiers: []model.PackageQualifierInputSpec{
-				{Key: "arch", Value: "amd64"},
+var (
+	inmems         = "inmem"
+	topns          = "topns"
+	topns1         = "topns1"
+	topns2         = "topns2"
+	topns3         = "topns3"
+	ns             = "ubuntu"
+	version        = "1.19.0"
+	depns          = "openssl.org"
+	depns2         = "openssl.org2"
+	opensslVersion = "3.0.3"
+	IsDepTests     = assembler.IngestPredicates{
+		IsDependency: []assembler.IsDependencyIngest{
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "deb",
+					Namespace: &ns,
+					Name:      "dpkg",
+					Version:   &version,
+					Qualifiers: []model.PackageQualifierInputSpec{
+						{Key: "arch", Value: "amd64"},
+					},
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "conan",
+					Namespace: &depns,
+					Name:      "openssl",
+					Version:   &opensslVersion,
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   "=>1.19.0",
+					DependencyType: model.DependencyTypeDirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "top",
+					Namespace: &topns,
+					Name:      "toppkg",
+					Version:   &version,
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "deb",
+					Namespace: &ns,
+					Name:      "dpkg",
+					Version:   &version,
+					Qualifiers: []model.PackageQualifierInputSpec{
+						{Key: "arch", Value: "amd64"},
+					},
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   "=>1.19.0",
+					DependencyType: model.DependencyTypeDirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "top1",
+					Namespace: &topns1,
+					Name:      "toppkg1",
+					Version:   &version,
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "top",
+					Namespace: &topns,
+					Name:      "toppkg",
+					Version:   &version,
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   "=>1.19.0",
+					DependencyType: model.DependencyTypeDirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "top2",
+					Namespace: &topns2,
+					Name:      "toppkg2",
+					Version:   &version,
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "top",
+					Namespace: &topns,
+					Name:      "toppkg",
+					Version:   &version,
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   "=>1.19.0",
+					DependencyType: model.DependencyTypeIndirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "top3",
+					Namespace: &topns3,
+					Name:      "toppkg3",
+					Version:   &version,
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "top",
+					Namespace: &topns,
+					Name:      "toppkg",
+					Version:   &version,
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   "<1.19.0",
+					DependencyType: model.DependencyTypeDirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "deb",
+					Namespace: &ns,
+					Name:      "dpkg",
+					Version:   &version,
+					Qualifiers: []model.PackageQualifierInputSpec{
+						{Key: "arch", Value: "amd64"},
+					},
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "conan2",
+					Namespace: &depns2,
+					Name:      "openssl2",
+					Version:   &opensslVersion,
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   "=>1.19.0",
+					DependencyType: model.DependencyTypeDirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
 			},
 		},
-		depPkg1: model.PkgInputSpec{
-			Type:      "conan",
-			Namespace: &depns,
-			Name:      "openssl",
-			Version:   &opensslVersion,
-		},
-		depPkg2: model.PkgInputSpec{
-			Type:      "conan2",
-			Namespace: &depns2,
-			Name:      "openssl2",
-			Version:   &opensslVersion,
-		},
-		dependencyDirect: model.IsDependencyInputSpec{
-			VersionRange:   "=>1.19.0",
-			DependencyType: model.DependencyTypeDirect,
-			Justification:  "test justification one",
-			Origin:         "Demo ingestion",
-			Collector:      "Demo ingestion",
-		},
-		dependencyNotInRange: model.IsDependencyInputSpec{
-			VersionRange:   "<1.19.0",
-			DependencyType: model.DependencyTypeIndirect,
-			Justification:  "test justification one",
-			Origin:         "Demo ingestion",
-			Collector:      "Demo ingestion",
-		},
-		dependencyIndirect: model.IsDependencyInputSpec{
-			VersionRange:   "=>1.19.0",
-			DependencyType: model.DependencyTypeIndirect,
-			Justification:  "test justification one",
-			Origin:         "Demo ingestion",
-			Collector:      "Demo ingestion",
-		},
-	}}
-	for _, ingest := range ingestDependencies {
-		_, err1 := model.IsDependency(context.Background(), client, ingest.topPkg, ingest.pkg, ingest.dependencyDirect)
-		if err1 != nil {
-			logger.Errorf("Error in ingesting: %v\n", err1)
-		}
-		_, err2 := model.IsDependency(context.Background(), client, ingest.pkg, ingest.depPkg1, ingest.dependencyDirect)
-		if err2 != nil {
-			logger.Errorf("Error in ingesting: %v\n", err2)
-		}
-		_, err3 := model.IsDependency(context.Background(), client, ingest.pkg, ingest.depPkg2, ingest.dependencyDirect)
-		if err3 != nil {
-			logger.Errorf("Error in ingesting: %v\n", err3)
-		}
-		_, err4 := model.IsDependency(context.Background(), client, ingest.topTopPkg1, ingest.topPkg, ingest.dependencyDirect)
-		if err4 != nil {
-			logger.Errorf("Error in ingesting: %v\n", err4)
-		}
-		_, err5 := model.IsDependency(context.Background(), client, ingest.topTopPkg2, ingest.topPkg, ingest.dependencyIndirect)
-		if err5 != nil {
-			logger.Errorf("Error in ingesting: %v\n", err5)
-		}
-		_, err6 := model.IsDependency(context.Background(), client, ingest.topTopPkg3, ingest.topPkg, ingest.dependencyNotInRange)
-		if err6 != nil {
-			logger.Errorf("Error in ingesting: %v\n", err5)
+	}
+)
+
+func ingestIsDependencyTestData(ctx context.Context, client graphql.Client) {
+	logger := logging.FromContext(ctx)
+	for _, ingest := range IsDepTests.IsDependency {
+		_, err := model.IsDependency(context.Background(), client, *ingest.Pkg, *ingest.DepPkg, *ingest.IsDependency)
+		if err != nil {
+			logger.Errorf("Error in ingesting: %v\n", err)
 		}
 	}
 }
 
 func Test_SearchSubgraphFromVuln(t *testing.T) {
-	type1 := "deb"
-	ns1 := "ubuntu"
-	nodeName1 := "dpkg"
-	type2 := "top"
-	ns2 := "topns"
-	nodeName2 := "toppkg"
-	type3 := "top1"
-	ns3 := "topns1"
-	nodeName3 := "toppkg1"
-	type4 := "top2"
-	ns4 := "topns2"
-	nodeName4 := "toppkg2"
-	type5 := "top3"
-	ns5 := "topns3"
-	nodeName5 := "toppkg3"
-
 	server, logger := startTestServer()
 	ctx := logging.WithLogger(context.Background())
 
 	httpClient := http.Client{}
 	gqlclient := graphql.NewClient("http://localhost:9090/query", &httpClient)
 
-	ingestTestData(ctx, gqlclient)
+	ingestIsDependencyTestData(ctx, gqlclient)
 
-	// filtering for packages IDs needed to be inputted into the tests
-	pkgFilter1 := &model.PkgSpec{
-		Type:      &type1,
-		Namespace: &ns1,
-		Name:      &nodeName1,
+	type pkgFilterVar struct {
+		pkgType         string
+		pkgNamespace    string
+		pkgName         string
+		depPkgType      string
+		depPkgNamespace string
+		depPkgName      string
 	}
 
-	pkgResponse1, err := model.Packages(ctx, gqlclient, pkgFilter1)
+	var pkgIds []string
 
-	if err != nil {
-		t.Errorf("Error getting first package node for test case: %s\n", err)
+	for _, dep := range IsDepTests.IsDependency {
+		pkgFilter := &model.PkgSpec{
+			Type:      &dep.Pkg.Type,
+			Namespace: dep.Pkg.Namespace,
+			Name:      &dep.Pkg.Name,
+		}
+		pkgResponse, err := model.Packages(ctx, gqlclient, pkgFilter)
+
+		if err != nil {
+			t.Errorf("Error getting id for isDep test case: %s\n", err)
+		}
+
+		pkgIds = append(pkgIds, pkgResponse.Packages[0].Namespaces[0].Names[0].Versions[0].Id)
 	}
-
-	id1 := pkgResponse1.Packages[0].Namespaces[0].Names[0].Versions[0].Id
-
-	pkgFilter2 := &model.PkgSpec{
-		Type:      &type2,
-		Namespace: &ns2,
-		Name:      &nodeName2,
-	}
-
-	pkgResponse2, err := model.Packages(ctx, gqlclient, pkgFilter2)
-
-	if err != nil {
-		t.Errorf("Error getting second package node for test case: %s\n", err)
-	}
-
-	id2 := pkgResponse2.Packages[0].Namespaces[0].Names[0].Versions[0].Id
-
-	pkgFilter3 := &model.PkgSpec{
-		Type:      &type3,
-		Namespace: &ns3,
-		Name:      &nodeName3,
-	}
-
-	pkgResponse3, err := model.Packages(ctx, gqlclient, pkgFilter3)
-
-	if err != nil {
-		t.Errorf("Error getting third package node for test case: %s\n", err)
-	}
-
-	id3 := pkgResponse3.Packages[0].Namespaces[0].Names[0].Versions[0].Id
-
-	pkgFilter4 := &model.PkgSpec{
-		Type:      &type4,
-		Namespace: &ns4,
-		Name:      &nodeName4,
-	}
-
-	pkgResponse4, err := model.Packages(ctx, gqlclient, pkgFilter4)
-
-	if err != nil {
-		t.Errorf("Error getting fourth package node for test case: %s\n", err)
-	}
-
-	id4 := pkgResponse4.Packages[0].Namespaces[0].Names[0].Versions[0].Id
-
-	pkgFilter5 := &model.PkgSpec{
-		Type:      &type5,
-		Namespace: &ns5,
-		Name:      &nodeName5,
-	}
-
-	pkgResponse5, err := model.Packages(ctx, gqlclient, pkgFilter5)
-
-	if err != nil {
-		t.Errorf("Error getting fifth package node for  test case: %s\n", err)
-	}
-
-	id5 := pkgResponse5.Packages[0].Namespaces[0].Names[0].Versions[0].Id
 
 	testCases := []struct {
 		startID  string
@@ -260,44 +237,38 @@ func Test_SearchSubgraphFromVuln(t *testing.T) {
 		maxDepth int
 	}{
 		{
-			// test case with two dependencies at the same depth, no stopID and no limiting maxDepth
-			startID:  id1,
+			// 1: test case with two dependencies at the same depth, no stopID and no limiting maxDepth
+			startID:  pkgIds[0],
 			stopID:   "",
 			maxDepth: 10,
 		},
 		{
-			// test case with two levels of dependencies, no stopID and no limiting maxDepth
-			startID:  id2,
+			// 2: test case with two levels of dependencies, no stopID and no limiting maxDepth
+			startID:  pkgIds[1],
 			stopID:   "",
 			maxDepth: 10,
 		},
 		{
-			// test case with two levels of dependencies, a stopID at the first level and no limiting maxDepth
-			startID:  id2,
-			stopID:   id1,
+			// 3: test case with two levels of dependencies, a stopID at the first level and no limiting maxDepth
+			startID:  pkgIds[1],
+			stopID:   pkgIds[0],
 			maxDepth: 10,
 		},
 		{
-			// test case with two levels of dependencies, no stopID and a limiting maxDepth at the first level
-			startID:  id3,
+			// 4: test case with two levels of dependencies, no stopID and a limiting maxDepth at the first level
+			startID:  pkgIds[2],
 			stopID:   "",
 			maxDepth: 1,
 		},
 		{
-			// test case with two levels of dependencies, no stopID and a limiting maxDepth at the first level
-			startID:  id3,
+			// 5: test case with indirect dependency
+			startID:  pkgIds[3],
 			stopID:   "",
 			maxDepth: 1,
 		},
 		{
-			// test case with indirect dependency
-			startID:  id4,
-			stopID:   "",
-			maxDepth: 1,
-		},
-		{
-			// test case with isDep range that does not include the dependency
-			startID:  id5,
+			// 6: test case with isDep range that does not include the dependency
+			startID:  pkgIds[4],
 			stopID:   "",
 			maxDepth: 10,
 		},
@@ -319,85 +290,91 @@ func Test_SearchSubgraphFromVuln(t *testing.T) {
 				}
 			}
 
-			if tt.startID == id1 {
+			// Test Case 1
+			if tt.startID == pkgIds[0] {
 				if diff := cmp.Diff(3, len(gotMap)); len(diff) > 0 {
 					t.Errorf("Number of map entries (-want +got):\n%s", diff)
 				}
 
 				for k, v := range gotMap {
-					if k != id1 && (v.parent != id1 || v.depth != 1) {
+					if k != pkgIds[0] && (v.parent != pkgIds[0] || v.depth != 1) {
 						t.Errorf("Incorrect dependency node entry")
-					} else if k == id1 && (v.parent != "" || v.depth != 0) {
+					} else if k == pkgIds[0] && (v.parent != "" || v.depth != 0) {
 						t.Errorf("Incorrect starting node entry")
 					}
 				}
 			}
 
-			if tt.startID == id2 && tt.stopID == "" && tt.maxDepth == 10 {
+			// Test Case 2
+			if tt.startID == pkgIds[1] && tt.stopID == "" && tt.maxDepth == 10 {
 				if diff := cmp.Diff(4, len(gotMap)); len(diff) > 0 {
 					t.Errorf("Number of map entries (-want +got):\n%s", diff)
 				}
 
 				for k, v := range gotMap {
-					if k == id2 && (v.parent != "" || v.depth != 0) {
+					if k == pkgIds[1] && (v.parent != "" || v.depth != 0) {
 						t.Errorf("Incorrect starting node entry")
-					} else if k == id1 && (v.parent != id2 || v.depth != 1) {
+					} else if k == pkgIds[0] && (v.parent != pkgIds[1] || v.depth != 1) {
 						t.Errorf("Incorrect second node entry")
-					} else if v.parent == id1 && v.depth != 2 {
+					} else if v.parent == pkgIds[0] && v.depth != 2 {
 						t.Errorf("Incorrect third or fourth node entry")
 					}
 				}
 			}
 
-			if tt.startID == id2 && tt.stopID == id1 {
+			// Test Case 3
+			if tt.startID == pkgIds[1] && tt.stopID == pkgIds[0] {
 				if diff := cmp.Diff(2, len(gotMap)); len(diff) > 0 {
 					t.Errorf("Number of map entries (-want +got):\n%s", diff)
 				}
 
 				for k, v := range gotMap {
-					if k == id2 && (v.parent != "" || v.depth != 0) {
+					if k == pkgIds[1] && (v.parent != "" || v.depth != 0) {
 						t.Errorf("Incorrect starting node entry")
-					} else if k == id1 && (v.parent != id2 || v.depth != 1) {
+					} else if k == pkgIds[0] && (v.parent != pkgIds[1] || v.depth != 1) {
 						t.Errorf("Incorrect second node entry")
 					}
 				}
 			}
 
-			if tt.startID == id3 {
+			// Test Case 4
+			if tt.startID == pkgIds[2] {
 				if diff := cmp.Diff(2, len(gotMap)); len(diff) > 0 {
 					t.Errorf("Number of map entries (-want +got):\n%s", diff)
 				}
 
 				for k, v := range gotMap {
-					if k == id3 && (v.parent != "" || v.depth != 0) {
+					if k == pkgIds[2] && (v.parent != "" || v.depth != 0) {
 						t.Errorf("Incorrect starting node entry")
-					} else if k == id2 && (v.parent != id3 || v.depth != 1) {
+					} else if k == pkgIds[1] && (v.parent != pkgIds[2] || v.depth != 1) {
 						t.Errorf("Incorrect second node entry")
 					}
 				}
 			}
 
-			if tt.startID == id4 {
+			// Test Case 5
+			if tt.startID == pkgIds[3] {
 				if diff := cmp.Diff(2, len(gotMap)); len(diff) > 0 {
 					t.Errorf("Number of map entries (-want +got):\n%s", diff)
 				}
 
 				for k, v := range gotMap {
-					if k == id4 && (v.parent != "" || v.depth != 0) {
+					if k == pkgIds[3] && (v.parent != "" || v.depth != 0) {
 						t.Errorf("Incorrect starting node entry")
-					} else if k == id2 && (v.parent != id4 || v.depth != 1) {
+					} else if k == pkgIds[1] && (v.parent != pkgIds[3] || v.depth != 1) {
 						t.Errorf("Incorrect second node entry")
 					}
 				}
 			}
 
-			if tt.startID == id5 {
+			// Test Case 6
+			if tt.startID == pkgIds[4] {
 				if diff := cmp.Diff(1, len(gotMap)); len(diff) > 0 {
 					t.Errorf("Number of map entries (-want +got):\n%s", diff)
 				}
 
 				for k, v := range gotMap {
-					if k == id1 && (v.parent != "" || v.depth != 0) {
+					if k == pkgIds[0] && (v.parent != "" || v.depth != 0) {
 						t.Errorf("Incorrect starting node entry")
 					}
 				}
