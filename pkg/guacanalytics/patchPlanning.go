952bdacd3d907d0258e367451aa993c2b1632d8b
Rebecca Metzman <49292050+rmetzman@users.noreply.github.com>
2023-07-07 16:15:59 -0400

    Implement search dependent packages + testing (updated) (#969)
    * adding files
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * simplifying tests
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * making public
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing errors in test
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * renaming files
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * casing in patchPlanning
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing unsued
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * formatting
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * changes
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * removing variables
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * small fixes
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * generalizing
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * simplify tests
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * ingest seperated in tests
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * adding comments to test
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * resolving comments
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * small
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * changing bac
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * changing casing
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing error
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * removing print statenment
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixinf error with ingesting on git
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * error fixing
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * testing if old version passes tests
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * reverting revert
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * trying to fix ingestion
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * trying old commit
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * changing back
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * trying small test case
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * changing ingestion to align with new PR
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * adding more description error message
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * resolving PR comments
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * adding extra switch to case
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing the switch
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * removing global variables
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing nit
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * changing comments
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * changing q
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing tests
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * working tests
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing lint
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * resolving lint
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * fixing pointer optional
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * adding to a comment
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    * resolving  nits
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    
    ---------
    
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>
    Co-authored-by: Rebecca Metzman <rmetzman@google.com>

---
+++ pkg/guacanalytics/patchPlanning.go
@@ -0,0 +1,192 @@
+//
+// Copyright 2023 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package guacanalytics
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Khan/genqlient/graphql"
+	model "github.com/guacsec/guac/pkg/assembler/clients/generated"
+	"github.com/guacsec/guac/pkg/misc/depversion"
+)
+
+type DfsNode struct {
+	Expanded     bool // true once all node neighbors are added to queue
+	Parent       string
+	Depth        int
+	NodeType     string   // packageName, packageVersion
+	nodeVersions []string // for a packageName, what was the packageVersion associated with this version
+}
+
+type queueValues struct {
+	nodeMap map[string]DfsNode
+	now     string
+	nowNode DfsNode
+	queue   []string
+}
+
+// TODO: make more robust using predicates
+func SearchDependenciesFromStartNode(ctx context.Context, gqlclient graphql.Client, startID string, stopID *string, maxDepth int) (map[string]DfsNode, error) {
+	startNode, err := model.Node(ctx, gqlclient, startID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed getting intial node with given ID:%w", err)
+	}
+
+	nodePkg, ok := startNode.Node.(*model.NodeNodePackage)
+
+	if !ok {
+		return nil, fmt.Errorf("Not a package")
+	}
+
+	q := queueValues{
+		queue:   make([]string, 0), // the queue of nodes in bfs
+		nodeMap: map[string]DfsNode{},
+	}
+
+	// TODO: add functionality to start with other nodes?
+	if len(nodePkg.AllPkgTree.Namespaces) < 1 {
+		return nil, fmt.Errorf("Start by inputting a packageName or packageVerion node")
+	}
+
+	if len(nodePkg.AllPkgTree.Namespaces[0].Names) < 1 {
+		return nil, fmt.Errorf("Start by inputting a packageName or packageVerion node")
+	}
+
+	if len(nodePkg.AllPkgTree.Namespaces[0].Names[0].Versions) < 1 {
+		// TODO: handle case where there are circular depedencies that introduce more versions to the version list on a node that requires revisiting
+		var versionsList []string
+		for _, versionEntry := range nodePkg.AllPkgTree.Namespaces[0].Names[0].Versions {
+			versionsList = append(versionsList, versionEntry.Version)
+		}
+		q.nodeMap[startID] = DfsNode{
+			NodeType:     "packageName",
+			nodeVersions: versionsList,
+		}
+	} else {
+		q.nodeMap[startID] = DfsNode{
+			NodeType: "packageVersion",
+		}
+
+		// Add packageName node to the frontier as well
+		q.queue = append(q.queue, nodePkg.AllPkgTree.Namespaces[0].Names[0].Id)
+
+		var versionsList []string
+		versionsList = append(versionsList, nodePkg.AllPkgTree.Namespaces[0].Names[0].Versions[0].Version)
+		q.nodeMap[nodePkg.AllPkgTree.Namespaces[0].Names[0].Id] = DfsNode{
+			NodeType:     "packageName",
+			nodeVersions: versionsList,
+		}
+	}
+
+	q.queue = append(q.queue, startID)
+
+	for len(q.queue) > 0 {
+		q.now = q.queue[0]
+		q.queue = q.queue[1:]
+		q.nowNode = q.nodeMap[q.now]
+
+		if stopID != nil && *stopID == q.now {
+			break
+		}
+
+		if q.nowNode.Depth >= maxDepth {
+			break
+		}
+
+		neighborsResponse, err := model.Neighbors(ctx, gqlclient, q.now, []model.Edge{})
+
+		if err != nil {
+			return nil, fmt.Errorf("failed getting neighbors:%w", err)
+		}
+
+		for _, neighbor := range neighborsResponse.Neighbors {
+			err = caseOnPredicates(ctx, gqlclient, &q, neighbor, q.nowNode.NodeType)
+
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		q.nowNode.Expanded = true
+		q.nodeMap[q.now] = q.nowNode
+	}
+
+	return q.nodeMap, nil
+
+}
+
+func caseOnPredicates(ctx context.Context, gqlclient graphql.Client, q *queueValues, neighbor model.NeighborsNeighborsNode, nodeType string) error {
+	// case on predicates and nodeType
+	switch nodeType {
+	case "packageName":
+		switch neighbor := neighbor.(type) {
+		case *model.NeighborsNeighborsIsDependency:
+			err := exploreIsDependencyFromDepPkg(ctx, gqlclient, q, *neighbor)
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func exploreIsDependencyFromDepPkg(ctx context.Context, gqlclient graphql.Client, q *queueValues, isDependency model.NeighborsNeighborsIsDependency) error {
+	doesRangeInclude, err := depversion.DoesRangeInclude(q.nowNode.nodeVersions, isDependency.VersionRange)
+
+	if err != nil {
+		return err
+	}
+
+	if !doesRangeInclude {
+		return nil
+	}
+
+	dfsNVersion, seenVersion := q.nodeMap[isDependency.Package.Namespaces[0].Names[0].Versions[0].Id]
+	dfsNName, seenName := q.nodeMap[isDependency.Package.Namespaces[0].Names[0].Id]
+
+	if !seenVersion {
+		dfsNVersion = DfsNode{
+			Parent:   q.now,
+			Depth:    q.nowNode.Depth + 1,
+			NodeType: "packageVersion",
+		}
+		q.nodeMap[isDependency.Package.Namespaces[0].Names[0].Versions[0].Id] = dfsNVersion
+	}
+
+	if !seenName {
+		dfsNName = DfsNode{
+			Parent:       q.now,
+			Depth:        q.nowNode.Depth + 1,
+			NodeType:     "packageName",
+			nodeVersions: []string{isDependency.Package.Namespaces[0].Names[0].Versions[0].Version},
+		}
+		q.nodeMap[isDependency.Package.Namespaces[0].Names[0].Id] = dfsNName
+	}
+
+	if !dfsNVersion.Expanded {
+		q.queue = append(q.queue, isDependency.Package.Namespaces[0].Names[0].Versions[0].Id)
+	}
+
+	if !dfsNName.Expanded {
+		q.queue = append(q.queue, isDependency.Package.Namespaces[0].Names[0].Id)
+	}
+
+	return nil
+}
---
+++ pkg/guacanalytics/patchPlanning_test.go
@@ -0,0 +1,485 @@
+//
+// Copyright 2023 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package guacanalytics
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/Khan/genqlient/graphql"
+	"github.com/google/go-cmp/cmp"
+	"github.com/guacsec/guac/internal/testing/ptrfrom"
+	"github.com/guacsec/guac/pkg/assembler"
+	"github.com/guacsec/guac/pkg/assembler/backends/inmem"
+	model "github.com/guacsec/guac/pkg/assembler/clients/generated"
+	"github.com/guacsec/guac/pkg/assembler/graphql/generated"
+	"github.com/guacsec/guac/pkg/assembler/graphql/resolvers"
+	"github.com/guacsec/guac/pkg/logging"
+)
+
+var (
+	isDependencySimpleGraph = assembler.IngestPredicates{
+
+		IsDependency: []assembler.IsDependencyIngest{
+
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "deb",
+					Namespace: ptrfrom.String("ubuntu"),
+					Name:      "dpkg",
+					Version:   ptrfrom.String("1.19.0"),
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "conan",
+					Namespace: ptrfrom.String("openssl.org"),
+					Name:      "openssl",
+					Version:   ptrfrom.String("3.0.3"),
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   ">=1.19.0",
+					DependencyType: model.DependencyTypeDirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "top",
+					Namespace: ptrfrom.String("topns"),
+					Name:      "toppkg",
+					Version:   ptrfrom.String("1.19.0"),
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "deb",
+					Namespace: ptrfrom.String("ubuntu"),
+					Name:      "dpkg",
+					Version:   ptrfrom.String("1.19.0"),
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   ">=1.19.0",
+					DependencyType: model.DependencyTypeDirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "conan",
+					Namespace: ptrfrom.String("openssl.org"),
+					Name:      "openssl",
+					Version:   ptrfrom.String("3.0.3"),
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "bottom",
+					Namespace: ptrfrom.String("bottomns"),
+					Name:      "bottompkg",
+					Version:   ptrfrom.String("1.19.0"),
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   ">=1.19.0",
+					DependencyType: model.DependencyTypeIndirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+		},
+	}
+	isDependencyNotInRangeGraph = assembler.IngestPredicates{
+		IsDependency: []assembler.IsDependencyIngest{
+			{
+				Pkg: &model.PkgInputSpec{
+					Type:      "conan3",
+					Namespace: ptrfrom.String("openssl.org3"),
+					Name:      "openssl3",
+					Version:   ptrfrom.String("3.0.3"),
+				},
+				DepPkg: &model.PkgInputSpec{
+					Type:      "extraType",
+					Namespace: ptrfrom.String("extraNamespace"),
+					Name:      "extraName",
+					Version:   ptrfrom.String("1.19.0"),
+				},
+				IsDependency: &model.IsDependencyInputSpec{
+					VersionRange:   "=3.0.3",
+					DependencyType: model.DependencyTypeDirect,
+					Justification:  "test justification one",
+					Origin:         "Demo ingestion",
+					Collector:      "Demo ingestion",
+				},
+			},
+		},
+	}
+)
+
+func ingestIsDependency(ctx context.Context, client graphql.Client, graph assembler.IngestPredicates) error {
+
+	for _, ingest := range graph.IsDependency {
+		_, err := model.IngestPackage(context.Background(), client, *ingest.Pkg)
+
+		if err != nil {
+			return fmt.Errorf("Error in ingesting package: %s\n", err)
+		}
+
+		_, err = model.IngestPackage(context.Background(), client, *ingest.DepPkg)
+
+		if err != nil {
+			return fmt.Errorf("Error in ingesting dependent package: %s\n", err)
+		}
+		_, err = model.IsDependency(context.Background(), client, *ingest.Pkg, *ingest.DepPkg, *ingest.IsDependency)
+
+		if err != nil {
+			return fmt.Errorf("Error in ingesting isDependency: %s\n", err)
+		}
+	}
+	return nil
+}
+
+func ingestTestData(ctx context.Context, client graphql.Client, graphInput string) error {
+	switch graphInput {
+	case "isDependencySimpleGraph":
+		err := ingestIsDependency(ctx, client, isDependencySimpleGraph)
+		if err != nil {
+			return err
+		}
+		return nil
+	case "isDependencyNotInRangeGraph":
+		err := ingestIsDependency(ctx, client, isDependencyNotInRangeGraph)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return fmt.Errorf("Graph input did not match any test graph")
+}
+
+func Test_SearchSubgraphFromVuln(t *testing.T) {
+	server, err := startTestServer()
+
+	if err != nil {
+		t.Errorf("Error starting server: %s \n", err)
+		os.Exit(1)
+	}
+
+	ctx := logging.WithLogger(context.Background())
+
+	httpClient := http.Client{}
+	gqlclient := graphql.NewClient("http://localhost:9090/query", &httpClient)
+
+	testCases := []struct {
+		name           string
+		startType      string
+		startNamespace string
+		startName      string
+		startVersion   *string
+		stopType       *string
+		stopNamespace  string
+		stopName       string
+		stopVersion    *string
+		maxDepth       int
+		expectedLen    int
+		expectedPkgs   []string
+		graphInput     string
+	}{
+		{
+			name:           "1: two levels of dependencies, no stopID and no limiting maxDepth",
+			startType:      "conan",
+			startNamespace: "openssl.org",
+			startName:      "openssl",
+			startVersion:   ptrfrom.String("3.0.3"),
+			maxDepth:       10,
+			expectedLen:    6,
+			expectedPkgs:   []string{"top", "deb", "conan"},
+			graphInput:     "isDependencySimpleGraph",
+		},
+		{
+			name:           "2:  one level of dependencies, no stopID and no limiting maxDepth",
+			startType:      "deb",
+			startNamespace: "ubuntu",
+			startName:      "dpkg",
+			startVersion:   ptrfrom.String("1.19.0"),
+			maxDepth:       10,
+			expectedLen:    4,
+			expectedPkgs:   []string{"top", "deb"},
+			graphInput:     "isDependencySimpleGraph",
+		},
+		{
+			name:           "3: two levels of dependencies, a stopID at the first level and no limiting maxDepth",
+			startType:      "conan",
+			startNamespace: "openssl.org",
+			startName:      "openssl",
+			startVersion:   ptrfrom.String("3.0.3"),
+			stopType:       ptrfrom.String("deb"),
+			stopNamespace:  "ubuntu",
+			stopName:       "dpkg",
+			stopVersion:    ptrfrom.String("1.19.0"),
+			maxDepth:       10,
+			expectedLen:    4,
+			expectedPkgs:   []string{"deb", "conan"},
+			graphInput:     "isDependencySimpleGraph",
+		},
+		{
+			name:           "4: two levels of dependencies, no stopID and a limiting maxDepth at the first level",
+			startType:      "conan",
+			startNamespace: "openssl.org",
+			startName:      "openssl",
+			startVersion:   ptrfrom.String("3.0.3"),
+			maxDepth:       1,
+			expectedLen:    4,
+			expectedPkgs:   []string{"deb", "conan"},
+			graphInput:     "isDependencySimpleGraph",
+		},
+		{
+			name:           "5: isDependency indirect dependency",
+			startType:      "bottom",
+			startNamespace: "bottomns",
+			startName:      "bottompkg",
+			startVersion:   ptrfrom.String("1.19.0"),
+			maxDepth:       10,
+			expectedLen:    8,
+			expectedPkgs:   []string{"top", "deb", "conan", "bottom"},
+			graphInput:     "isDependencySimpleGraph",
+		},
+		{
+			name:           "6: isDependency no dependents returns no extra",
+			startType:      "top",
+			startNamespace: "topns",
+			startName:      "toppkg",
+			startVersion:   ptrfrom.String("1.19.0"),
+			maxDepth:       10,
+			expectedLen:    2,
+			expectedPkgs:   []string{"top"},
+			graphInput:     "isDependencySimpleGraph",
+		},
+		{
+			name:           "7: direct isDependency not included in range",
+			startType:      "extraType",
+			startNamespace: "extraNamespace",
+			startName:      "extraName",
+			startVersion:   ptrfrom.String("1.19.0"),
+			maxDepth:       10,
+			expectedLen:    2,
+			expectedPkgs:   []string{"extraType"},
+			graphInput:     "isDependencyNotInRangeGraph",
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(fmt.Sprintf("Test case %s\n", tt.name), func(t *testing.T) {
+			err = ingestTestData(ctx, gqlclient, tt.graphInput)
+
+			if err != nil {
+				t.Errorf("Error ingesting test data: %s", err)
+				return
+			}
+
+			var getPackageIDsValues []*string
+			var startID string
+			if tt.startVersion != nil {
+				getPackageIDsValues, err = getPackageIDs(ctx, gqlclient, ptrfrom.String(tt.startType), tt.startNamespace, tt.startName, tt.startVersion, true, false)
+			} else {
+				getPackageIDsValues, err = getPackageIDs(ctx, gqlclient, ptrfrom.String(tt.startType), tt.startNamespace, tt.startName, nil, false, true)
+			}
+
+			if err != nil {
+				t.Errorf("Error finding startNode: %s", err)
+				return
+			}
+
+			if getPackageIDsValues == nil || len(getPackageIDsValues) > 1 {
+				t.Errorf("Cannot locate matching startID input\n")
+				return
+			}
+
+			startID = *getPackageIDsValues[0]
+
+			var stopID *string
+			if tt.stopType != nil {
+				if tt.stopVersion != nil {
+					getPackageIDsValues, err = getPackageIDs(ctx, gqlclient, tt.stopType, tt.stopNamespace, tt.stopName, tt.stopVersion, true, false)
+				} else {
+					getPackageIDsValues, err = getPackageIDs(ctx, gqlclient, tt.stopType, tt.stopNamespace, tt.stopName, nil, false, true)
+				}
+
+				if err != nil {
+					t.Errorf("Error finding stopNode: %s", err)
+					return
+				}
+
+				if getPackageIDsValues == nil || len(getPackageIDsValues) > 1 {
+					t.Errorf("Cannot locate matching stopID input\n")
+					return
+				}
+
+				stopID = getPackageIDsValues[0]
+			}
+
+			gotMap, err := SearchDependenciesFromStartNode(ctx, gqlclient, startID, stopID, tt.maxDepth)
+
+			if err != nil {
+				t.Errorf("got err from SearchDependenciesFromStartNode: %s", err)
+			}
+
+			if diff := cmp.Diff(tt.expectedLen, len(gotMap)); len(diff) > 0 {
+				t.Errorf("Number of map entries (-want +got):\n%s", diff)
+			}
+
+			var expectedIDs []string
+			for _, pkg := range tt.expectedPkgs {
+				pkgIDs, err := getPackageIDs(ctx, gqlclient, &pkg, "", "", nil, false, false)
+				if err != nil {
+					t.Errorf("Expected package %s not found: %s\n", pkg, err)
+				}
+
+				for _, ID := range pkgIDs {
+					expectedIDs = append(expectedIDs, *ID)
+				}
+
+				// only applicable to cases with one version node
+				if len(pkgIDs) == 2 {
+					if gotMap[*pkgIDs[0]].NodeType == "" {
+						t.Errorf("First expected package %s is not in results\n", pkg)
+					}
+
+					if gotMap[*pkgIDs[1]].NodeType == "" {
+						t.Errorf("Second expected package %s is not in results\n", pkg)
+					}
+				}
+			}
+
+			for gotID, node := range gotMap {
+
+				if stopID == nil && tt.maxDepth == 10 {
+					if !node.Expanded {
+						t.Errorf("All nodes should be expanded but this node was not: node %s \n", gotID)
+					}
+				}
+				//check that other pkgs are not present in return map
+				inExpected := false
+				for _, expectedID := range expectedIDs {
+					if expectedID == gotID {
+						inExpected = true
+						break
+					}
+				}
+
+				if !inExpected {
+					t.Errorf("This ID appears in the returned map but is not expected: %s \n", gotID)
+					return
+				}
+			}
+
+		})
+	}
+
+	// cleaning up server instance
+	done := make(chan bool, 1)
+	ctx, cf := context.WithCancel(ctx)
+	go func() {
+		_ = server.Shutdown(ctx)
+		done <- true
+	}()
+	select {
+	case <-done:
+	case <-time.After(5 * time.Second):
+		cf()
+		server.Close()
+	}
+	cf()
+}
+
+func startTestServer() (*http.Server, error) {
+	ctx := logging.WithLogger(context.Background())
+	logger := logging.FromContext(ctx)
+
+	srv, err := getGraphqlTestServer()
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialize graphql server: %s", err)
+	}
+	http.Handle("/query", srv)
+
+	server := &http.Server{Addr: fmt.Sprintf(":%d", 9090)}
+	logger.Info("starting server")
+
+	go func() {
+		logger.Infof("server finished: %s", server.ListenAndServe())
+	}()
+	return server, nil
+}
+
+func getGraphqlTestServer() (*handler.Server, error) {
+	var topResolver resolvers.Resolver
+	args := inmem.DemoCredentials{}
+	backend, err := inmem.GetBackend(&args)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating inmem backend: %w", err)
+	}
+
+	topResolver = resolvers.Resolver{Backend: backend}
+
+	config := generated.Config{Resolvers: &topResolver}
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(config))
+
+	return srv, nil
+}
+
+// This function return matching packageName and/or packageVersion node IDs depending on if you specified to only find name nodes or version nodes
+func getPackageIDs(ctx context.Context, gqlclient graphql.Client, nodeType *string, nodeNamespace string, nodeName string, nodeVersion *string, justFindVersion bool, justFindName bool) ([]*string, error) {
+	var pkgFilter model.PkgSpec
+	if nodeVersion != nil {
+		pkgFilter = model.PkgSpec{
+			Type:      nodeType,
+			Namespace: &nodeNamespace,
+			Name:      &nodeName,
+			Version:   nodeVersion,
+		}
+	} else {
+		pkgFilter = model.PkgSpec{
+			Type: nodeType,
+		}
+	}
+	pkgResponse, err := model.Packages(ctx, gqlclient, &pkgFilter)
+
+	if err != nil {
+		return nil, fmt.Errorf("Error getting id for test case: %s\n", err)
+	}
+	var foundIDs []*string
+
+	if len(pkgResponse.Packages[0].Namespaces[0].Names) > 0 && !justFindVersion {
+		for _, name := range pkgResponse.Packages[0].Namespaces[0].Names {
+			foundIDs = append(foundIDs, &name.Id)
+		}
+	}
+
+	if len(pkgResponse.Packages[0].Namespaces[0].Names[0].Versions) > 0 && !justFindName {
+		for _, version := range pkgResponse.Packages[0].Namespaces[0].Names[0].Versions {
+			foundIDs = append(foundIDs, &version.Id)
+		}
+	}
+
+	if len(foundIDs) < 1 {
+		return nil, fmt.Errorf("No matching nodes found\n")
+	}
+
+	return foundIDs, nil
+}
