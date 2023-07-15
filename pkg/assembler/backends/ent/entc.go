922ff79562a714b77e3b3c0442ab97aadecf7023
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2023-07-16 10:48:55 +1200

    Add ent/contrib gql support
    Signed-off-by: Ivan Vanderbyl <ivanvanderbyl@gmail.com>

---
+++ pkg/assembler/backends/ent/entc.go
@@ -0,0 +1,26 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"log"
+
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
+)
+
+func main() {
+	ex, err := entgql.NewExtension()
+	if err != nil {
+		log.Fatalf("creating entgql extension: %v", err)
+	}
+	if err := entc.Generate("./schema", &gen.Config{
+		Features: []gen.Feature{
+			gen.FeatureUpsert,
+		},
+	}, entc.Extensions(ex)); err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+}
