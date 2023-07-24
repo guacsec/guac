6b5e5949ebd68d1cfbabd1cff625d08b88526620
Rebecca Metzman <rmetzman@google.com>
2023-07-24 14:30:11 +0000

    trying to find fields
    Signed-off-by: Rebecca Metzman <rmetzman@google.com>

--- pkg/guacanalytics/patchPlanning.go
+++ pkg/guacanalytics/patchPlanning.go
@@ -276,12 +276,18 @@ func exploreHasSourceAtFromPackage(ctx context.Context, gqlClient graphql.Client
 
 // TODO: implement
 func explorePointOfContact(ctx context.Context, gqlClient graphql.Client, q *queueValues, pointOfContact model.NeighborsNeighborsPointOfContact) {
+	pocFilter := model.PointOfContactSpec{
+		Subject: now,
+	}
+
+	pointOfContact.AllPointOfContact
+
 	// Step 1: Add field to current node in nodeMap of this POC (may need to copy over old fields)
 	q.nodeMap[q.now] = BfsNode{
 		Parent: q.nowNode.Parent,
 		Depth:  q.nowNode.Depth,
 	}
-	pointOfContact.Subject
+
 	// Step 2: If it is a packageName, add the POC to applicable versions (versions in the nodeVersions) but not the reverse
 	// (i.e. for a version do not add POC to associated name as it may not be applicable)
 }
