9dce0e01915cb0d091571917ffeb9346ad97da84
stevemenezes <steve.menezes@yahooinc.com>
2023-08-25 12:45:25 -0700

    adds license info and removes unreachable code
    Signed-off-by: stevemenezes <steve.menezes@yahooinc.com>

--- pkg/handler/processor/cdx_vex/cdx_vex.go
+++ pkg/handler/processor/cdx_vex/cdx_vex.go
@@ -1,3 +1,18 @@
+//
+// Copyright 2023 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cdx_vex
 
 import (
@@ -26,9 +41,7 @@ func (p *CdxVexProcessor) ValidateSchema(d *processor.Document) error {
 		return err
 	}
 
-	return fmt.Errorf("unable to support parsing of CSAF document format: %v", d.Format)
-
-	return nil
+	return fmt.Errorf("unable to support parsing of cdx-vex document format: %v", d.Format)
 }
 
 func (p *CdxVexProcessor) Unpack(d *processor.Document) ([]*processor.Document, error) {
