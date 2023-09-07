39c8bb17425277791a314a3bb827367c5b259412
Soham Arora <arorasoham9@gmail.com>
2023-09-07 11:08:57 +0000

    removing ent changes

--- pkg/assembler/backends/ent/backend/pkgequal.go
+++ pkg/assembler/backends/ent/backend/pkgequal.go
@@ -41,15 +41,15 @@ func (b *EntBackend) PkgEqual(ctx context.Context, spec *model.PkgEqualSpec) ([]
 	return collect(records, toModelPkgEqual), nil
 }
 
-func (b *EntBackend) IngestPkgEqual(ctx context.Context, pkg model.PkgInputSpec, depPkg model.PkgInputSpec, pkgEqual model.PkgEqualInputSpec) (string, error) {
+func (b *EntBackend) IngestPkgEqual(ctx context.Context, pkg model.PkgInputSpec, depPkg model.PkgInputSpec, pkgEqual model.PkgEqualInputSpec) (*model.PkgEqual, error) {
 	record, err := WithinTX(ctx, b.client, func(ctx context.Context) (*ent.PkgEqual, error) {
 		return upsertPackageEqual(ctx, ent.TxFromContext(ctx), pkg, depPkg, pkgEqual)
 	})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return toModelPkgEqual(record).ID, nil
+	return toModelPkgEqual(record), nil
 }
 
 func upsertPackageEqual(ctx context.Context, client *ent.Tx, pkgA model.PkgInputSpec, pkgB model.PkgInputSpec, spec model.PkgEqualInputSpec) (*ent.PkgEqual, error) {
