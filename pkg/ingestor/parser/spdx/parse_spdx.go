3d704a37209385d25fde46edb695962ee56e8040
m-brophy <mbrophy@redhat.com>
2023-10-02 12:56:32 +0100

    formatting changes
    Signed-off-by: m-brophy <mbrophy@redhat.com>

--- pkg/ingestor/parser/spdx/parse_spdx.go
+++ pkg/ingestor/parser/spdx/parse_spdx.go
@@ -431,7 +431,7 @@ func isDependent(rel string) bool {
 
 func isPackageOf(rel string) bool {
 	return map[string]bool{
-		spdx_common.TypeRelationshipPackageOf:  true,
+		spdx_common.TypeRelationshipPackageOf: true,
 	}[rel]
 }
 
