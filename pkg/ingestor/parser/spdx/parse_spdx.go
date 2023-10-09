09c5879d013471a22f1aadc5f0ed6eb9ee7075b9
Mark Brophy <36955467+m-brophy@users.noreply.github.com>
2023-10-09 19:07:09 +0100

    process `PACKAGE_OF` relationship in SPDX files (#1337)
    * process `PACKAGE_OF` relationship in SPDX files
    
    Signed-off-by: m-brophy <mbrophy@redhat.com>
    
    * formatting changes
    
    Signed-off-by: m-brophy <mbrophy@redhat.com>
    
    * process `PACKAGE_OF` relationship in SPDX files
    
    Signed-off-by: m-brophy <mbrophy@redhat.com>
    
    * formatting changes
    
    Signed-off-by: m-brophy <mbrophy@redhat.com>
    
    * add new test for PACKAGE_OF relationship ingestion
    
    Signed-off-by: m-brophy <mbrophy@redhat.com>
    
    ---------
    
    Signed-off-by: m-brophy <mbrophy@redhat.com>

--- pkg/ingestor/parser/spdx/parse_spdx.go
+++ pkg/ingestor/parser/spdx/parse_spdx.go
@@ -265,7 +265,7 @@ func (s *spdxParser) GetPredicates(ctx context.Context) *assembler.IngestPredica
 		if isDependency(rel.Relationship) {
 			foundId = string(rel.RefA.ElementRefID)
 			relatedId = string(rel.RefB.ElementRefID)
-		} else if isDependent(rel.Relationship) {
+		} else if isDependent(rel.Relationship) || isPackageOf(rel.Relationship) {
 			foundId = string(rel.RefB.ElementRefID)
 			relatedId = string(rel.RefA.ElementRefID)
 		} else {
@@ -423,6 +423,12 @@ func isDependent(rel string) bool {
 	return map[string]bool{
 		spdx_common.TypeRelationshipContainedBy:  true,
 		spdx_common.TypeRelationshipDependencyOf: true,
+	}[rel]
+}
+
+func isPackageOf(rel string) bool {
+	return map[string]bool{
+		spdx_common.TypeRelationshipPackageOf: true,
 	}[rel]
 }
 
