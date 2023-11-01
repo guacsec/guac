5a1bea637ac834baaddacdb9d9f9ead2e12cba66
Höhl, Lukas <lukas.hoehl@accso.de>
2023-11-01 12:01:21 +0100

    chore(oci-collector): lint fixes and fmt
    Signed-off-by: Höhl, Lukas <lukas.hoehl@accso.de>

--- pkg/handler/collector/oci/attestation_collector.go
+++ pkg/handler/collector/oci/attestation_collector.go
@@ -1,8 +1,24 @@
+//
+// Copyright * The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the \"License\");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an \"AS IS\" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package oci
 
 import (
 	"context"
 	"fmt"
+
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/guacsec/guac/pkg/handler/processor"
@@ -14,6 +30,9 @@ type attestationCollector struct{}
 
 func (c *attestationCollector) Collect(ctx context.Context, ref name.Reference, docChannel chan<- *processor.Document, opts ...remote.Option) error {
 	signedEntity, err := cosign_remote.SignedEntity(ref, cosign_remote.WithRemoteOptions(opts...))
+	if err != nil {
+		return err
+	}
 	atts, err := signedEntity.Attestations()
 	if err != nil {
 		return err
