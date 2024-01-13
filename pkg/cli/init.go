8509e90472ca26ea0e9a3f3ccddf92ea0d47a8cf
naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>
2024-01-12 18:03:14 -0600

    Included prometheus server for guacql
    - Included prometheus for guacql
    - Will include in all other
      https://github.com/guacsec/guac/pull/1500#issuecomment-1823130996 if
      this is acceptable.
    
    Signed-off-by: naveensrinivasan <172697+naveensrinivasan@users.noreply.github.com>

--- pkg/cli/init.go
+++ pkg/cli/init.go
@@ -17,18 +17,21 @@ package cli
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/guacsec/guac/pkg/logging"
-
+	"github.com/guacsec/guac/pkg/metrics"
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 func InitConfig() {
-
 	home, err := homedir.Dir()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to get user home directory: %v\n", err)
@@ -66,4 +69,41 @@ func InitConfig() {
 	if err == nil {
 		logger.Infof("Using config file: %s", viper.ConfigFileUsed())
 	}
+}
+
+// SetupPrometheus sets up the prometheus server
+func SetupPrometheus(ctx context.Context, logger *zap.SugaredLogger, name string) error {
+	if name == "" {
+		return errors.New("name cannot be empty")
+	}
+	m := metrics.FromContext(ctx, name)
+	enablePrometheus := viper.GetBool("enable-prometheus")
+	prometheusPort := viper.GetInt("prometheus-addr")
+	if !enablePrometheus {
+		return nil
+	}
+
+	go func() {
+		http.Handle("/metrics", m.MetricsHandler())
+		logger.Infof("Prometheus server is listening on: %d", prometheusPort)
+		server := &http.Server{Addr: fmt.Sprintf(":%d", prometheusPort)}
+
+		// Start server in a goroutine so that it doesn't block
+		go func() {
+			if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				logger.Fatalf("Error starting HTTP server: %v", err)
+			}
+		}()
+
+		// Listen for the cancellation signal
+		<-ctx.Done()
+
+		// Shutdown the server when cancellation signal is received
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			logger.Errorf("Error shutting down server: %v", err)
+		}
+	}()
+	return nil
 }
