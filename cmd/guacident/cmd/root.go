5563ac0eb49a621fade3abad44643d8833c3a7b2
Soham Arora <arorasoham9@gmail.com>
2024-02-07 05:49:21 -0500

    fixing headers
    Signed-off-by: Soham Arora <arorasoham9@gmail.com>

--- cmd/guacident/cmd/root.go
+++ cmd/guacident/cmd/root.go
@@ -1,6 +1,18 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
+//
+// Copyright 2022 The GUAC Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cmd
 
 import (
@@ -97,25 +109,3 @@ func truncate(s string, length int) string {
 	return s
 }
 
-func stringify(data []string) string {
-	str := ""
-	for _, d := range data {
-		str += d + ","
-	}
-	return str[:len(str)-1] // Remove the trailing comma
-}
-func printJSON(data interface{}, indent int) {
-    switch v := data.(type) {
-    case map[string]interface{}:
-        for key, val := range v {
-            fmt.Printf("%*s%s:\n", indent*4, "", key)
-            printJSON(val, indent+1)
-        }
-    case []interface{}:
-        for _, val := range v {
-            printJSON(val, indent)
-        }
-    default:
-        fmt.Printf("%*s%v\n", indent*4, "", v)
-    }
-}
