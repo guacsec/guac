5ff7adb6fe2afcaac54db19853510b5a992e55fe
nathannaveen <42319948+nathannaveen@users.noreply.github.com>
2024-02-26 13:08:43 -0600

    Updated tests
    Signed-off-by: nathannaveen <42319948+nathannaveen@users.noreply.github.com>

--- internal/testing/backend/search_test.go
+++ internal/testing/backend/search_test.go
@@ -26,20 +26,7 @@ func TestFindSoftware(t *testing.T) {
 			name:       "default package",
 			InPkg:      []*model.PkgInputSpec{testdata.P1},
 			searchText: "tensorflow",
-			want: []model.PackageSourceOrArtifact{
-				model.Package{
-					Type: "pypi",
-					Namespaces: []*model.PackageNamespace{{
-						Names: []*model.PackageName{{
-							Name: "tensorflow",
-							Versions: []*model.PackageVersion{{
-								Version:    "",
-								Qualifiers: []*model.PackageQualifier{},
-							}},
-						}},
-					},
-					},
-				}},
+			want:       []model.PackageSourceOrArtifact{*testdata.P1out},
 		},
 		{
 			name:       "package no match",
@@ -52,7 +39,7 @@ func TestFindSoftware(t *testing.T) {
 			InArt: []*model.ArtifactInputSpec{
 				{
 					Algorithm: "sha256",
-					Digest:    "testdigest",
+					Digest:    "testdigest", // using a custom digest, so we aren't using testdata.A1
 				},
 			},
 			searchText: "test",
@@ -75,38 +62,53 @@ func TestFindSoftware(t *testing.T) {
 			want:       []model.PackageSourceOrArtifact{},
 		},
 		{
-			name: "default source",
-			InSrc: []*model.SourceInputSpec{
+			name:       "default source",
+			InSrc:      []*model.SourceInputSpec{testdata.S1},
+			searchText: "jeff",
+			want:       []model.PackageSourceOrArtifact{*testdata.S1out},
+		},
+		{
+			name:       "source no match",
+			InSrc:      []*model.SourceInputSpec{testdata.S1},
+			searchText: "invalid",
+			want:       []model.PackageSourceOrArtifact{},
+		},
+		{
+			name: "source and package match",
+			InPkg: []*model.PkgInputSpec{
 				{
-					Type:      "git",
-					Namespace: "github.com/jeff",
-					Name:      "myrepo",
+					Type: "p",
+					Name: "sourceAndPackageName",
 				},
 			},
-			searchText: "jeff",
+			InSrc: []*model.SourceInputSpec{
+				{
+					Type:      "s",
+					Namespace: "testSourceNamespace",
+					Name:      "sourceAndPackageName",
+				},
+			},
+			searchText: "sourceAndPackage",
 			want: []model.PackageSourceOrArtifact{
+				model.Package{
+					Type: "p",
+					Namespaces: []*model.PackageNamespace{{
+						Names: []*model.PackageName{{
+							Name:     "sourceAndPackageName",
+							Versions: []*model.PackageVersion{{}},
+						}},
+					}},
+				},
 				model.Source{
-					Type: "git",
+					Type: "s",
 					Namespaces: []*model.SourceNamespace{{
-						Namespace: "github.com/jeff",
+						Namespace: "testSourceNamespace",
 						Names: []*model.SourceName{{
-							Name: "myrepo",
+							Name: "sourceAndPackageName",
 						}},
 					}},
 				},
 			},
-		},
-		{
-			name: "source no match",
-			InSrc: []*model.SourceInputSpec{
-				{
-					Type:      "git",
-					Namespace: "github.com/jeff",
-					Name:      "myrepo",
-				},
-			},
-			searchText: "invalid",
-			want:       []model.PackageSourceOrArtifact{},
 		},
 	}
 	for _, test := range tests {
