ac4c273f738c622a3aa9c88160945d6c2676e3b5
Nathan Naveen <42319948+nathannaveen@users.noreply.github.com>
2024-03-08 14:33:32 -0600

    Include missing collectors (#1759)
    * Included S3 collector
    
    Signed-off-by: nathannaveen <42319948+nathannaveen@users.noreply.github.com>
    
    * Included GCS collector
    
    Signed-off-by: nathannaveen <42319948+nathannaveen@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: nathannaveen <42319948+nathannaveen@users.noreply.github.com>

---
+++ cmd/guaccollect/cmd/gcs.go
@@ -0,0 +1,130 @@
+package cmd
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"fmt"
+	"github.com/guacsec/guac/pkg/cli"
+	"github.com/guacsec/guac/pkg/collectsub/client"
+	csub_client "github.com/guacsec/guac/pkg/collectsub/client"
+	"github.com/guacsec/guac/pkg/handler/collector"
+	"github.com/guacsec/guac/pkg/handler/collector/gcs"
+	"github.com/guacsec/guac/pkg/logging"
+	"github.com/guacsec/guac/pkg/version"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"google.golang.org/api/option"
+	"os"
+)
+
+type gcsOptions struct {
+	pubSubAddr        string
+	blobAddr          string
+	graphqlEndpoint   string
+	csubClientOptions client.CsubClientOptions
+	bucket            string
+}
+
+const gcsCredentialsPathFlag = "gcp-credentials-path"
+
+var gcsCmd = &cobra.Command{
+	Use:     "gcs [flags] bucket_name",
+	Short:   "takes SBOMs and attestations from a Google Cloud Storage bucket and injects them to GUAC graph. This command talks directly to the graphQL endpoint",
+	Example: "guacone collect gcs my-bucket --gcs-credentials-path /secret/sa.json",
+	Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := logging.WithLogger(context.Background())
+		logger := logging.FromContext(ctx)
+
+		opts, err := validateGCSFlags(
+			viper.GetString("pubsub-addr"),
+			viper.GetString("blob-addr"),
+			viper.GetString("gql-addr"),
+			viper.GetString("csub-addr"),
+			viper.GetBool("csub-tls"),
+			viper.GetBool("csub-tls-skip-verify"),
+			viper.GetString(gcsCredentialsPathFlag),
+			args)
+		if err != nil {
+			fmt.Printf("unable to validate flags: %v\n", err)
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+
+		gcsOpts := []option.ClientOption{
+			option.WithUserAgent(version.UserAgent),
+		}
+
+		// Credential flag is not mandatory since they can also be loaded from
+		// the environment variable GOOGLE_APPLICATION_CREDENTIALS by the client, by default
+		if credsPath := viper.GetString(gcsCredentialsPathFlag); credsPath != "" {
+			gcsOpts = append(gcsOpts, option.WithCredentialsFile(credsPath))
+		}
+
+		client, err := storage.NewClient(ctx, gcsOpts...)
+		if err != nil {
+			logger.Fatalf("creating client: %v", err)
+		}
+
+		// Register collector by providing a new GCS Client and bucket name
+		gcsCollector, err := gcs.NewGCSCollector(gcs.WithBucket(opts.bucket), gcs.WithClient(client))
+		if err != nil {
+			logger.Fatalf("unable to create gcs client: %v", err)
+		}
+
+		err = collector.RegisterDocumentCollector(gcsCollector, gcs.CollectorGCS)
+		if err != nil {
+			logger.Fatalf("unable to register gcs collector: %v", err)
+		}
+
+		// initialize collectsub client
+		csubClient, err := csub_client.NewClient(opts.csubClientOptions)
+		if err != nil {
+			logger.Infof("collectsub client initialization failed, this ingestion will not pull in any additional data through the collectsub service: %v", err)
+			csubClient = nil
+		} else {
+			defer csubClient.Close()
+		}
+
+		initializeNATsandCollector(ctx, opts.pubSubAddr, opts.blobAddr)
+	},
+}
+
+func validateGCSFlags(pubSubAddr, blobAddr, gqlEndpoint string, csubAddr string, csubTls bool, csubTlsSkipVerify bool, credentialsPath string, args []string) (gcsOptions, error) {
+	opts := gcsOptions{
+		pubSubAddr:      pubSubAddr,
+		blobAddr:        blobAddr,
+		graphqlEndpoint: gqlEndpoint,
+	}
+
+	csubOpts, err := client.ValidateCsubClientFlags(csubAddr, csubTls, csubTlsSkipVerify)
+	if err != nil {
+		return opts, fmt.Errorf("unable to validate csub client flags: %w", err)
+	}
+	opts.csubClientOptions = csubOpts
+
+	if len(args) < 1 {
+		return opts, fmt.Errorf("expected positional argument: bucket")
+	}
+	opts.bucket = args[0]
+
+	if credentialsPath == "" && os.Getenv("GOOGLE_APPLICATION_CREDENTIALS") == "" {
+		return opts, fmt.Errorf("expected either --%s flag or GOOGLE_APPLICATION_CREDENTIALS environment variable", gcsCredentialsPathFlag)
+	}
+
+	return opts, nil
+}
+
+func init() {
+	set, err := cli.BuildFlags([]string{gcsCredentialsPathFlag})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to setup flag: %v", err)
+		os.Exit(1)
+	}
+	gcsCmd.PersistentFlags().AddFlagSet(set)
+	if err := viper.BindPFlags(gcsCmd.PersistentFlags()); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to bind flags: %v", err)
+		os.Exit(1)
+	}
+	rootCmd.AddCommand(gcsCmd)
+}
---
+++ cmd/guaccollect/cmd/s3.go
@@ -0,0 +1,168 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"github.com/guacsec/guac/pkg/cli"
+	csub_client "github.com/guacsec/guac/pkg/collectsub/client"
+	"github.com/guacsec/guac/pkg/handler/collector"
+	"github.com/guacsec/guac/pkg/handler/collector/s3"
+	"github.com/guacsec/guac/pkg/logging"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// s3Options flags for configuring the command
+type s3Options struct {
+	pubSubAddr        string                        // address for the publisher/subscriber to connect to
+	blobAddr          string                        // address for the blob store to connect to
+	s3url             string                        // base url of the s3 to collect from
+	s3bucket          string                        // name of bucket to collect from
+	s3item            string                        // s3 item (only for non-polling behaviour)
+	region            string                        // AWS region, for s3/sqs configuration (defaults to us-east-1)
+	queues            string                        // comma-separated list of queues/topics (only for polling behaviour)
+	mp                string                        // message provider name (sqs or kafka, will default to kafka)
+	mpEndpoint        string                        // endpoint for the message provider (only for polling behaviour)
+	poll              bool                          // polling or non-polling behaviour? (defaults to non-polling)
+	graphqlEndpoint   string                        // endpoint for the graphql server
+	csubClientOptions csub_client.CsubClientOptions // options for the collectsub client
+}
+
+var s3Cmd = &cobra.Command{
+	Use:   "s3 [flags]",
+	Short: "takes SBOMs and attestations from S3 compatible bucket and injects them to GUAC graph",
+	Long: `S3 collector can download one item from the storage, the whole bucket or listen to storage events using sqs/kafka (poll) and download the files as they are uploaded.
+Make sure that access credentials variables are properly set.`,
+	Example: `Create example bucket:
+
+$ mc mb play/guac-test
+$ mc cp --recursive internal/testing/testdata/exampledata/* play/guac-test
+
+Set access variables:
+
+$ export AWS_ACCESS_KEY_ID=Q3AM3UQ867SPQQA43P2Fe
+$ export AWS_SECRET_ACCESS_KEY=zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG
+
+Ingest:
+
+$ guacone collect s3 --s3-url https://play.min.io --s3-bucket guac-test
+$ guacone collect s3 --s3-url play.min.io --s3-bucket guac-test --s3-item alpine-cyclonedx.json
+
+For the polling option, you need to define event bus endpoint for bucket notifications:
+
+$ guacone collect s3 --s3-url http://localhost:9000 --s3-bucket guac-test --poll --s3-mp-endpoint localhost:9092 --s3-queues sboms
+	`,
+	Args: cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := logging.WithLogger(context.Background())
+		logger := logging.FromContext(ctx)
+
+		s3Opts, err := validateS3Opts(
+			viper.GetString("pubsub-addr"),
+			viper.GetString("blob-addr"),
+			viper.GetString("gql-addr"),
+			viper.GetString("csub-addr"),
+			viper.GetBool("csub-tls"),
+			viper.GetBool("csub-tls-skip-verify"),
+			viper.GetString("s3-url"),
+			viper.GetString("s3-bucket"),
+			viper.GetString("s3-region"),
+			viper.GetString("s3-item"),
+			viper.GetString("s3-mp"),
+			viper.GetString("s3-mp-endpoint"),
+			viper.GetString("s3-queues"),
+			viper.GetBool("poll"),
+		)
+		if err != nil {
+			fmt.Printf("failed to validate flags: %v\n", err)
+			_ = cmd.Help()
+			os.Exit(1)
+		}
+
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+		s3Collector := s3.NewS3Collector(s3.S3CollectorConfig{
+			S3Url:                   s3Opts.s3url,
+			S3Bucket:                s3Opts.s3bucket,
+			S3Region:                s3Opts.region,
+			S3Item:                  s3Opts.s3item,
+			MessageProvider:         s3Opts.mp,
+			MessageProviderEndpoint: s3Opts.mpEndpoint,
+			Queues:                  s3Opts.queues,
+			Poll:                    s3Opts.poll,
+		})
+
+		if err := collector.RegisterDocumentCollector(s3Collector, s3.S3CollectorType); err != nil {
+			logger.Fatalf("unable to register s3 collector: %v\n", err)
+		}
+
+		csubClient, err := csub_client.NewClient(s3Opts.csubClientOptions)
+		if err != nil {
+			logger.Infof("collectsub client initialization failed, this ingestion will not pull in any additional data through the collectsub service: %v", err)
+			csubClient = nil
+		} else {
+			defer csubClient.Close()
+		}
+
+		initializeNATsandCollector(ctx, s3Opts.pubSubAddr, s3Opts.blobAddr)
+	},
+}
+
+func validateS3Opts(pubSubAddr, blobAddr, graphqlEndpoint, csubAddr string, csubTls, csubTlsSkipVerify bool, s3url, s3bucket, region, s3item, mp, mpEndpoint, queues string, poll bool) (s3Options, error) {
+	var opts s3Options
+
+	if poll {
+		if mp == "kafka" {
+			if len(mpEndpoint) == 0 {
+				return opts, fmt.Errorf("expected endpoint for message provider")
+			}
+		}
+		if len(queues) == 0 {
+			return opts, fmt.Errorf("expected at least one queue")
+		}
+	}
+
+	if len(s3bucket) == 0 {
+		return opts, fmt.Errorf("expected s3 bucket")
+	}
+
+	csubClientOptions, err := csub_client.ValidateCsubClientFlags(csubAddr, csubTls, csubTlsSkipVerify)
+	if err != nil {
+		return opts, fmt.Errorf("unable to validate csub client flags: %w", err)
+	}
+
+	opts = s3Options{
+		pubSubAddr:        pubSubAddr,
+		blobAddr:          blobAddr,
+		s3url:             s3url,
+		s3bucket:          s3bucket,
+		s3item:            s3item,
+		region:            region,
+		queues:            queues,
+		mp:                mp,
+		mpEndpoint:        mpEndpoint,
+		poll:              poll,
+		graphqlEndpoint:   graphqlEndpoint,
+		csubClientOptions: csubClientOptions,
+	}
+
+	return opts, nil
+}
+
+func init() {
+	set, err := cli.BuildFlags([]string{"s3-url", "s3-bucket", "s3-item", "s3-region", "s3-queues", "s3-mp", "s3-mp-endpoint"})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to setup flag: %v", err)
+		os.Exit(1)
+	}
+	s3Cmd.PersistentFlags().AddFlagSet(set)
+	if err := viper.BindPFlags(s3Cmd.PersistentFlags()); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to bind flags: %v", err)
+		os.Exit(1)
+	}
+	rootCmd.AddCommand(s3Cmd)
+}
