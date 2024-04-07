fef4aecfb88cb605d1ca4ef419e06f14324aa0a6
Eszter Szucs-Matyas <eszter.szucs.matyas@gmail.com>
2024-04-07 15:52:06 +0200

    Add missing dev tools to nix shell
    Signed-off-by: Eszter Szucs-Matyas <eszter.szucs.matyas@gmail.com>

--- shell.nix
+++ shell.nix
@@ -8,10 +8,13 @@ mkShell {
     jq  
     gcc
     go_1_21
+    golangci-lint
     gopls
     gotests
     goreleaser
     nats-server
     protobuf
+    protoc-gen-go
+    protoc-gen-go-grpc
   ];
 }
