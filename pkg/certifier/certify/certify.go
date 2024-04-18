0cb1cebea5722621518819bebe9d4a43446209cc
Narsimham Chelluri (Narsa) <narsa@kusari.dev>
2024-04-18 14:38:41 -0300

    Remove unused variable
    - This fixes a golangci-lint unused linter failure.
    
    Signed-off-by: Narsimham Chelluri (Narsa) <narsa@kusari.dev>

--- pkg/certifier/certify/certify.go
+++ pkg/certifier/certify/certify.go
@@ -20,15 +20,11 @@ import (
 	"fmt"
 	"time"
 
-	jsoniter "github.com/json-iterator/go"
-
 	"github.com/guacsec/guac/pkg/certifier"
 	"github.com/guacsec/guac/pkg/handler/collector"
 	"github.com/guacsec/guac/pkg/handler/processor"
 	"github.com/guacsec/guac/pkg/logging"
 )
-
-var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 const (
 	BufferChannelSize int = 1000
