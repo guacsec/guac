782f037510d333bb310fd2cf04d6580374f3cae0
pxp928 <parth.psu@gmail.com>
2024-08-19 18:21:26 -0400

    add workflow on release to build atlas migration image
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- pkg/assembler/backends/ent/migrate/atlas.sh
+++ pkg/assembler/backends/ent/migrate/atlas.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+# Ensure all required environment variables are provided
+if [ -z "$PGUSER" ] || [ -z "$PGPASSWORD" ] || [ -z "$PGHOST" ] || [ -z "$PGPORT" ] || [ -z "$PGDATABASE" ]; then
+  echo "One or more required environment variables (PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE) are missing."
+  exit 1
+fi
+
 atlas migrate apply --dir file:///app/migrations --url "postgres://$PGUSER:$PGPASSWORD@$PGHOST:$PGPORT/$PGDATABASE?sslmode=disable"
