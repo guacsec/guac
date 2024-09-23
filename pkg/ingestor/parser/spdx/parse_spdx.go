d192b083f5fedbad898a798df8b2413041f8be51
pxp928 <parth.psu@gmail.com>
2024-09-23 15:24:26 -0400

    spdx add occur for top level artifact/package
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- pkg/ingestor/parser/spdx/parse_spdx.go
+++ pkg/ingestor/parser/spdx/parse_spdx.go
@@ -267,10 +267,12 @@ func (s *spdxParser) GetPredicates(ctx context.Context) *assembler.IngestPredica
 		}
 
 		if len(s.topLevelArtifacts) > 0 {
-			for _, arts := range s.topLevelArtifacts {
+			for id, arts := range s.topLevelArtifacts {
 				for _, art := range arts {
 					preds.HasSBOM = append(preds.HasSBOM, common.CreateTopLevelHasSBOMFromArtifact(art, s.doc, s.spdxDoc.DocumentNamespace, timestamp))
 				}
+				// append to packageArtifacts so that isOccurrence is created
+				s.packageArtifacts[id] = append(s.packageArtifacts[id], arts...)
 			}
 
 			if len(s.topLevelArtifacts) != len(s.topLevelPackages) {
