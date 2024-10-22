4e47bf7ea9a89b84e05109961816fe08aa6d6b8d
pxp928 <parth.psu@gmail.com>
2024-10-22 13:06:35 -0400

    add unit test for preloads helper
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- pkg/assembler/backends/helper/preloads_test.go
+++ pkg/assembler/backends/helper/preloads_test.go
@@ -1,5 +1,5 @@
 //
-// Copyright 2023 The GUAC Authors.
+// Copyright 2024 The GUAC Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,68 +21,33 @@ import (
 	"testing"
 
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/vektah/gqlparser/v2/ast"
 )
 
 func TestGetPreloads(t *testing.T) {
-	type args struct {
-		ctx context.Context
-	}
-	tests := []struct {
-		name string
-		args args
-		want []string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := GetPreloads(tt.args.ctx); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetPreloads() = %v, want %v", got, tt.want)
-			}
+	t.Run("Context with fields", func(t *testing.T) {
+		ctx := graphql.WithOperationContext(context.Background(), &graphql.OperationContext{})
+		ctx = graphql.WithFieldContext(ctx, &graphql.FieldContext{
+			Field: graphql.CollectedField{
+				Selections: ast.SelectionSet{
+					&ast.Field{
+						Name: "field_1",
+						SelectionSet: ast.SelectionSet{
+							&ast.Field{
+								Name: "field_a",
+							},
+						},
+					},
+					&ast.Field{
+						Name: "field_2",
+					},
+				},
+			},
 		})
-	}
-}
-
-func Test_getNestedPreloads(t *testing.T) {
-	type args struct {
-		ctx     *graphql.OperationContext
-		fields  []graphql.CollectedField
-		prefix  string
-		visited map[string]bool
-	}
-	tests := []struct {
-		name string
-		args args
-		want []string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getNestedPreloads(tt.args.ctx, tt.args.fields, tt.args.prefix, tt.args.visited); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("getNestedPreloads() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_getPreloadString(t *testing.T) {
-	type args struct {
-		prefix string
-		name   string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := getPreloadString(tt.args.prefix, tt.args.name); got != tt.want {
-				t.Errorf("getPreloadString() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+		result := GetPreloads(ctx)
+		expected := []string{"field_1", "field_1.field_a", "field_2"}
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("expected %v, got %v", expected, result)
+		}
+	})
 }
