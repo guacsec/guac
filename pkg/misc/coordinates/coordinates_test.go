37429e84738efded94c4f07957d31a9fdd6c13ae
pxp928 <parth.psu@gmail.com>
2024-11-05 15:50:08 -0500

    if namespace is empty string, change it to a hypen for the coordinates
    Signed-off-by: pxp928 <parth.psu@gmail.com>

--- pkg/misc/coordinates/coordinates_test.go
+++ pkg/misc/coordinates/coordinates_test.go
@@ -148,6 +148,18 @@ func TestConvertPurlToCoordinate(t *testing.T) {
 				Revision:       "244fd47e07d1004",
 			},
 			wantErr: false,
+		},
+		{
+			Name:    "github",
+			purlUri: "pkg:github/purl-spec@244fd47e07d1004#everybody/loves/dogs",
+			want: &Coordinate{
+				CoordinateType: "git",
+				Provider:       "github",
+				Namespace:      "-",
+				Name:           "purl-spec",
+				Revision:       "244fd47e07d1004",
+			},
+			wantErr: false,
 		}, {
 			Name:    "golang",
 			purlUri: "pkg:golang/cloud.google.com/go/compute@1.23.0",
@@ -157,6 +169,18 @@ func TestConvertPurlToCoordinate(t *testing.T) {
 				Namespace:      "cloud.google.com%2fgo",
 				Name:           "compute",
 				Revision:       "v1.23.0",
+			},
+			wantErr: false,
+		},
+		{
+			Name:    "golang",
+			purlUri: "pkg:golang/context@234fd47e07d1004f0aed9c#api",
+			want: &Coordinate{
+				CoordinateType: "go",
+				Provider:       "golang",
+				Namespace:      "-",
+				Name:           "context",
+				Revision:       "234fd47e07d1004f0aed9c",
 			},
 			wantErr: false,
 		}, {
@@ -234,7 +258,7 @@ func TestConvertPurlToCoordinate(t *testing.T) {
 				t.Errorf("ConvertPurlToCoordinate() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(*got, *tt.want) {
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ConvertPurlToCoordinate() = %v, want %v", got, tt.want)
 			}
 		})
