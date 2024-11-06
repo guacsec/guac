10712410d934e03705dbf02f21be81460ea68ff8
Lukas <lukas.hoehl.316@gmail.com>
2024-11-06 20:18:57 +0100

    Discard changes to pkg/ingestor/parser/parser.go
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.cloud>

--- pkg/ingestor/parser/parser.go
+++ pkg/ingestor/parser/parser.go
@@ -53,7 +53,9 @@ func init() {
 	_ = RegisterDocumentParser(opaque.NewOpaqueParser, processor.DocumentOpaque)
 }
 
-var documentParser = map[processor.DocumentType]func() common.DocumentParser{}
+var (
+	documentParser = map[processor.DocumentType]func() common.DocumentParser{}
+)
 
 type docTreeBuilder struct {
 	identities    []common.TrustInformation
